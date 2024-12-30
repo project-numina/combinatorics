7934a2ad0739417fe0397b655803ab8c356ec27f
ahhwuhu <liujunqi@amss.ac.cn>
2024-12-30 11:25:44 +0800

    finish statement of blueprint

--- Combinatorics/Basic.lean
+++ Combinatorics/Basic.lean
@@ -2,7 +2,7 @@ import Mathlib
 
 universe u v w
 
-theorem pigeonhole_simple {α : Type u} {β : Type v} [DecidableEq β] {s : Finset α} {t : Finset β}
+theorem thm3_1_1 {α : Type u} {β : Type v} [DecidableEq β] {s : Finset α} {t : Finset β}
     {f : α → β} (hf : ∀ a ∈ s, f a ∈ t) (hs : (s.image f).card < s.card) :
     ∃ y ∈ t, 1 < (Finset.filter (fun x ↦ f x = y) s).card := by
   by_contra! h
