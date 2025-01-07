0c7a7501dbbb3c508f4baa8decb746d9adf3de16
xbb <xbbng.0@gmail.com>
2025-01-07 13:55:18 +0800

    update

--- Combinatorics/PermutationsCombinations/Combinations.lean
+++ Combinatorics/PermutationsCombinations/Combinations.lean
@@ -11,9 +11,9 @@ Theorem 2.3.1 For 0 ≤ r ≤ n, P(n, r) = r! × C(n, r). Hence, C(n, r) = n! / 
 theorem  permutations_choose_Factorial  (r : ℕ) (n : Finset α) (h : r ≤ n.card) (hp: (permutationsLength r n).card = n.card.factorial / (n.card - r).factorial) :
   n.card.choose r = n.card.factorial / (r.factorial * (n.card - r).factorial)  := by sorry
 
--- theorem  permutations_choose_Factorial'  {n k : ℕ} (hk : k ≤ n) :
---     choose n k = n ! / (k ! * (n - k)!) :=
---   choose_eq_factorial_div_factorial hk
+theorem  permutations_choose_Factorial'  {n k : ℕ} (hk : k ≤ n) :
+    choose n k = n ! / (k ! * (n - k)!) :=
+  choose_eq_factorial_div_factorial hk
 
 
 /-
