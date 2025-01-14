1ade878d7f315025b7486607df141972fbebe997
xbb <xbbng.0@gmail.com>
2025-01-14 14:59:27 +0800

    update

--- Combinatorics/PermutationsCombinations/Combinations.lean
+++ Combinatorics/PermutationsCombinations/Combinations.lean
@@ -1,26 +1,29 @@
-import Combinatorics.PermutationsCombinations.permutation
-import Mathlib.Data.Nat.Choose.Sum
+import Mathlib.Data.Finset.Powerset
+import Combinatorics.PermutationsCombinations.Permutations
 
+open Nat Finset
 
-open Nat Finset BigOperators
+/-- Generate all possible subsets of size k from a given finite set s .
+-/
+def combinations (r : ℕ) (s : Finset α) : Finset (Finset α) :=
+  s.powersetCard r
 
+theorem combinations_card (r : ℕ) (s : Finset α) :
+  s.card.choose r = (combinations r s).card := by
+    rw [combinations]
+    exact (Finset.card_powersetCard r s).symm
 
 /--
 Theorem 2.3.1 For 0 ≤ r ≤ n, P(n, r) = r! × C(n, r). Hence, C(n, r) = n! / (r! × (n - r)!).
 -/
-theorem  permutations_choose_Factorial  (r : ℕ) (s : Finset α) (hp: (permutationsLength r s).card = s.card.factorial / (s.card - r).factorial) :
-  s.card.choose r = s.card.factorial / (r.factorial * (s.card - r).factorial)  := by sorry
-
-theorem  permutations_choose_Factorial'  {n k : ℕ} (hk : k ≤ n) :
-    choose n k = n ! / (k ! * (n - k)!) :=
-  choose_eq_factorial_div_factorial hk
+theorem  combinations_Factorial  (r : ℕ) (s : Finset α) (h : r ≤ s.card) (hp: (permutationsLength r s).card = r.factorial * (combinations r s).card) :
+  (combinations r s).card = (Finset.card s).factorial / (r.factorial * (Finset.card s - r).factorial) := by sorry
 
 
-/--
+/-
   Corollary 2.3.2 For 0 ≤ r ≤ n, C(n, n - k) = C(n, k)
 -/
-lemma choose_sub_symm {n k : ℕ} (hk : k ≤ n) : choose n (n - k) = choose n k :=
-  Nat.choose_symm hk
+-- This theorem has already been formalized in Mathlib under the name Nat.choose_symm.
 
 
 /--
@@ -28,12 +31,11 @@ lemma choose_sub_symm {n k : ℕ} (hk : k ≤ n) : choose n (n - k) = choose n k
 -/
 theorem Pascal_formula {n k : ℕ} (hk1 : 1 ≤ k)(hkn : k ≤ n - 1) :
   choose n k = choose (n - 1) (k - 1) + choose (n - 1) k := by
-  have hn : 0 < n := by omega
-  have hk : 0 < k := by omega
-  rw [Nat.choose_eq_choose_pred_add hn hk]
+  rw [Nat.choose_eq_choose_pred_add (show 0 < n by omega) (show 0 < k by omega)]
 
-/--
+
+
+/-
   Theorem 2.3.4 For n ≥ 0, C(n, 0) + C(n, 1) + ... + C(n, n) = 2^n.
 -/
-theorem sum_range_choose_two {n : ℕ} : ∑ k ∈  range (n + 1), choose n k  = 2 ^ n :=
-  Nat.sum_range_choose n
+-- This theorem has already been formalized in Mathlib under the name Nat.sum_range_choose.
