a06eb254669be303afff320ee9546dceb9d1fe04
ahhwuhu <liujunqi@amss.ac.cn>
2025-01-15 11:36:15 +0800

    create yaml and change a problem

---
+++ CombiBench/bxmo_2017_p2.lean
@@ -0,0 +1,7 @@
+import Mathlib
+
+/-- 214
+Let \\( n \\geqslant 2 \\) be an integer. Alice and Bob play a game concerning a country made of \\( n \\) islands. Exactly two of those \\( n \\) islands have a factory. Initially there is no bridge in the country. Alice and Bob take turns in the following way. In each turn, the player must build a bridge between two different islands \\( I_{1} \\) and \\( I_{2} \\) such that:\n\n- \\( I_{1} \\) and \\( I_{2} \\) are not already connected by a bridge;\n- at least one of the two islands \\( I_{1} \\) and \\( I_{2} \\) is connected by a series of bridges to an island with a factory (or has a factory itself). (Indeed, access to a factory is needed for the construction.)\n\nAs soon as a player builds a bridge that makes it possible to go from one factory to the other, this player loses the game. (Indeed, it triggers an industrial battle between both factories.) If Alice starts, then determine (for each \\( n \\geqslant 2 \\)) who has a winning strategy.\n\n(Note: It is allowed to construct a bridge passing above another bridge.)
+-/
+theorem bxmo_2017_p2 : 1 = 1
+    := by sorry
--- CombiBench/canadamo_2023_p2.lean
+++
@@ -1,7 +0,0 @@
-import Mathlib
-
-/-- 234
-There are 20 students in a high school class, and each student has exactly three close friends in the class. Five of the students have bought tickets to an upcoming concert. If any student sees that at least two of their close friends have bought tickets, then they will buy a ticket too.\n\nIs it possible that the entire class buys tickets to the concert?\n(Assume that friendship is mutual; if student $A$ is close friends with student $B$, then $B$ is close friends with $A$.)
--/
-theorem canadamo_2023_p2 : 1 = 1
-    := by sorry
