cdec882c556521531b1370321a51af7214bd31cc
ahhwuhu <liujunqi@amss.ac.cn>
2025-01-16 19:01:56 +0800

    fix imo name

--- CombiBench/imosl_2011_c6.lean
+++ CombiBench/imosl_2011_c6.lean
@@ -3,5 +3,5 @@ import Mathlib
 /-- 525
 Let $n$ be a positive integer and let $W=\\ldots x_{-1} x_{0} x_{1} x_{2} \\ldots$ be an infinite periodic word consisting of the letters $a$ and $b$. Suppose that the minimal period $N$ of $W$ is greater than $2^{n}$. A finite nonempty word $U$ is said to appear in $W$ if there exist indices $k \\leq \\ell$ such that $U=x_{k} x_{k+1} \\ldots x_{\\ell}$. A finite word $U$ is called ubiquitous if the four words $U a, U b, a U$, and $b U$ all appear in $W$. Prove that there are at least $n$ ubiquitous finite nonempty words.
 -/
-theorem imo_2011_c6 : 1 = 1
+theorem imosl_2011_c6 : 1 = 1
     := by sorry
--- CombiBench/imosl_2021_c5.lean
+++ CombiBench/imosl_2021_c5.lean
@@ -3,5 +3,5 @@ import Mathlib
 /-- 727
 Let $n$ and $k$ be two integers with $n > k \\geqslant 1$. There are $2n+1$ students standing in a circle. Each student $S$ has $2k$ neighbours - namely, the $k$ students closest to $S$ on the right, and the $k$ students closest to $S$ on the left. Suppose that $n+1$ of the students are girls, and the other $n$ are boys. Prove that there is a girl with at least $k$ girls among her neighbours.
 -/
-theorem imo_2021_c5 : 1 = 1
+theorem imosl_2021_c5 : 1 = 1
     := by sorry
