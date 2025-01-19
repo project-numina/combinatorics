5830e087509dc426982ef30aea0af7f7c1a7224d
ahhwuhu <liujunqi@amss.ac.cn>
2025-01-20 01:11:23 +0800

    add ten easy problem

---
+++ CombiBench/easy_4.lean
@@ -0,0 +1,7 @@
+import Mathlib
+
+/--
+A restaurant’s menu has 3 appetizers, 3 mains and 2 desserts. In how many way can a 3-course meal be ordered?
+-/
+theorem easy_4 : 1 = 1 := by
+    sorry
---
+++ CombiBench/easy_8.lean
@@ -0,0 +1,7 @@
+import Mathlib
+
+/--
+A ferry with a capacity of 10 people takes a group of 13 men and 7 women across a river. Find the number of waysin which the qroup may be taken across the if all women go on the first group.
+-/
+theorem easy_8 : 1 = 1 := by
+    sorry
