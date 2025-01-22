7c431e2d47ffb8d2ced851bee3763940160d8b74
ahhwuhu <liujunqi@amss.ac.cn>
2025-01-23 00:07:54 +0800

    easy_9_10

--- CombiBench/easy_10.lean
+++ CombiBench/easy_10.lean
@@ -1,7 +1,10 @@
 import Mathlib
 
+
+open Finset
 /--
 How many different ways can three people divide seven pears and five apples?
 -/
-theorem easy_10 : 1 = 1 := by
+theorem easy_10 : {(f : (Multiset.replicate 7 (1 : ℕ)) → range 3) | 1 = 1}.ncard *
+    {(f : (Multiset.replicate 5 (1 : ℕ)) → range 3) | 1 = 1}.ncard = 756 := by
     sorry
--- CombiBench/easy_9.lean
+++ CombiBench/easy_9.lean
@@ -1,7 +1,9 @@
 import Mathlib
 
+open Finset
 /--
 The father has six sons and ten identical, indistinguishable balls. How many ways can he give the balls to his sons if everyone gets at least one?
 -/
-theorem easy_9 : 1 = 1 := by
+theorem easy_9 :
+  {(f : (Multiset.replicate 10 (1 : ℕ)) → range 6) | ∀ y : range 6, ∃ x, f x = y}.ncard = 126 := by
     sorry
