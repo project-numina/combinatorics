31475c098761d9420b05dbc33008a829fd2f0e04
Beibei Xiong <155596312+BBXiong0520@users.noreply.github.com>
2025-01-23 10:34:42 +0800

    Update Easy_Cases.lean

--- Combinatorics/Easy_Cases.lean
+++ Combinatorics/Easy_Cases.lean
@@ -6,7 +6,7 @@ open List Finset Nat
 /--
 There are 8 athletes participating in a sprint competition. The referee needs to select 3 athletes and assign them specific rankings (first place, second place, and third place). How many different arrangements are possible?
 -/
-theorem case_12_v1: (Finset.permutationsLength 3 (range 8)).card = 336 := by
+theorem case_12_v1 : (Finset.permutationsLength 3 (range 8)).card = 336 := by
   rw [permutationsLength_card] <;> aesop
   
 
@@ -22,5 +22,5 @@ theorem easy_7 (x y : ℕ) :
 A ferry with a capacity of 10 people takes a group of 13 men and 7 women across a river.
   Find the number of waysin which the qroup may be taken across the if all women go on the first group.
   -/
-theorem easy_8 (s t :ℕ ) :
+theorem easy_8 (s t : ℕ) :
  ((powersetCard s ((range 7))).filter fun s ↦ s.card = 7).card * ((powersetCard t ((range 13))).filter fun t ↦ s + t.card = 10 ∧ s = 7 ).card= 286 := by sorry
