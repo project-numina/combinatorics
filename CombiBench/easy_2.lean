f1e12bce8a79f67d2d46b44fee74dc451f5988f9
Roman Soletskyi <32375425+romasoletskyi@users.noreply.github.com>
2025-02-04 07:41:47 +0100

    solutions (#14)

--- CombiBench/easy_2.lean
+++ CombiBench/easy_2.lean
@@ -1,7 +1,13 @@
-import Mathlib
+import Mathlib.Data.Fintype.Basic
+import Mathlib.Data.Fintype.Card
+import Combinatorics.PermutationsCombinations.Permutations
+
+open Finset
 
 /--
 There are 8 athletes participating in a sprint competition. The referee needs to select 3 athletes and assign them specific rankings (first place, second place, and third place). How many different arrangements are possible?
 -/
-theorem easy_2 : 1 = 1 := by
-    sorry
+theorem easy_2 : (permutationsLength 3 (@univ (Fin 8) _)).card = 336 := by
+  rw [permutationsLength_card]
+  . rfl
+  . simp
