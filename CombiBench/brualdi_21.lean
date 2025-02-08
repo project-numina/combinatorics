ec098bae4ef1062adff7412ed39b78f2110d776c
ahhwuhu <liujunqi@amss.ac.cn>
2025-02-08 19:19:25 +0800

    brualdi21-25

---
+++ CombiBench/brualdi_21.lean
@@ -0,0 +1,7 @@
+import Mathlib
+
+/--
+Formulate and prove a theorem that gives necessary and sufficient conditions so that a network with multiple sources and sinks has a flow that simultaneously meets all prescribed demands with available supplies.
+-/
+theorem brualdi_21 : 1 = 1 := by
+  sorry
---
+++ CombiBench/brualdi_23.lean
@@ -0,0 +1,9 @@
+import Mathlib
+
+def symmetry_gp : Affine.Triangle ℝ ℝ → Type :=
+  sorry
+/--
+Determine the symmetry group and corner-symmetry group of an equilateral triangle.
+-/
+theorem brualdi_23 : sorry = DihedralGroup 3 := by
+  sorry
