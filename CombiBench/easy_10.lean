c5f4b49fdd216da0f49ab14b40c82ce5f942cedc
zjj <zjj.zjj@zjj.zjj>
2025-02-11 12:31:27 +0000

    use finset

--- CombiBench/easy_10.lean
+++ CombiBench/easy_10.lean
@@ -1,11 +1,17 @@
 import Mathlib
+import Combinatorics.MyMultiset.Basic
 
 namespace easy_10
 
 open Finset
+
+
 /--
 How many different ways can three people divide seven pears and five apples?
 -/
-theorem easy_10 : {(f : (Multiset.replicate 7 (1 : ℕ)) → range 3) | 1 = 1}.ncard *
-    {(f : (Multiset.replicate 5 (1 : ℕ)) → range 3) | 1 = 1}.ncard = 756 := by
-    sorry
+theorem easy_10
+    (sols : Finset (Fin 3 → (ℕ × ℕ)))
+    (h_sols : ∀ f, f ∈ sols ↔ (∑ i, (f i).1 = 7 ∧ ∑ i, (f i).2 = 5)) :
+    sols.card = 756 := by sorry
+
+end easy_10
