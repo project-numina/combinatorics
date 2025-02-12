ae5e10a278e7d6a27ef0ec4bc49ce094749da024
ahhwuhu <liujunqi@amss.ac.cn>
2025-02-12 15:31:47 +0800

    delete 21 and 23

--- CombiBench/brualdi_23.lean
+++
@@ -1,8 +0,0 @@
-import Mathlib
-
-namespace brualdi_23
-/--
-Determine the symmetry group and corner-symmetry group of an equilateral triangle.
--/
-theorem brualdi_23 : sorry = DihedralGroup 3 := by
-  sorry
