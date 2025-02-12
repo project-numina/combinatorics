4c147b18d3fe1628b7b2ce5cc8591b2f46d2e773
97l <130048468+ahhwuhu@users.noreply.github.com>
2025-02-12 15:32:36 +0800

    [DRAFT] Brualdi 21 to 25 (#21)
    * easy_9_10
    
    * brualdi21-25
    
    * 21 and 23 dont finish
    
    * 2 easy and 3 brualdi and 1 imo
    
    * use finset
    
    * delete 21 and 23
    
    ---------
    
    Co-authored-by: zjj <zjj.zjj@zjj.zjj>

---
+++ CombiBench/brualdi_24.lean
@@ -0,0 +1,9 @@
+import Mathlib
+
+namespace brualdi_24
+/--
+Determine the generating function for the sequence of cubes \[ 0, 1, 8, \ldots, n^{3}, \ldots \]
+-/
+theorem brualdi_24 : PowerSeries.mk (fun (n : ℕ) => (n : ℝ) ^ 3) = PowerSeries.X *
+    (PowerSeries.X ^ 2 + 4 * PowerSeries.X + 1) * PowerSeries.inv (1 - PowerSeries.X) ^ 4 := by
+  sorry
--- CombiBench/easy_10.lean
+++ CombiBench/easy_10.lean
@@ -1,7 +1,17 @@
 import Mathlib
+import Combinatorics.MyMultiset.Basic
+
+namespace easy_10
+
+open Finset
+
 
 /--
 How many different ways can three people divide seven pears and five apples?
 -/
-theorem easy_10 : 1 = 1 := by
-    sorry
+theorem easy_10
+    (sols : Finset (Fin 3 → (ℕ × ℕ)))
+    (h_sols : ∀ f, f ∈ sols ↔ (∑ i, (f i).1 = 7 ∧ ∑ i, (f i).2 = 5)) :
+    sols.card = 756 := by sorry
+
+end easy_10
