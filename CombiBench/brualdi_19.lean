69e4d5955bb68422fa356135eeb6604b0c6984f7
97l <130048468+ahhwuhu@users.noreply.github.com>
2025-02-12 11:28:22 +0800

    Update and rename brualdi_20.lean to brualdi_19.lean

---
+++ CombiBench/brualdi_19.lean
@@ -0,0 +1,6 @@
+import Mathlib
+
+/-!
+Let $G$ be a graph. Prove that $G$ is 2-connected if and only if, for each vertex $x$ and each edge $\alpha$, there is a cycle that contains both the vertex $x$ and the edge $\alpha$.
+-/
+theorem brualdi_19 : 1 = 1 := by sorry
--- CombiBench/brualdi_20.lean
+++
@@ -1,7 +0,0 @@
-import Mathlib
-
-/-!
-Let $G$ be a graph. Prove that $G$ is 2-connected if and only if,
-for each vertex $x$ and each edge $\alpha$,
-there is a cycle that contains both the vertex $x$ and the edge $\alpha$.
--/
