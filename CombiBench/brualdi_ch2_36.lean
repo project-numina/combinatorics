acd312735ba3a8d8200d88860d31584297082f8e
ahhwuhu <liujunqi@amss.ac.cn>
2025-02-13 00:28:05 +0800

    rename

--- CombiBench/brualdi_ch2_36.lean
+++ CombiBench/brualdi_ch2_36.lean
@@ -1,10 +1,4 @@
-/-
-Determine the total number of combinations (of any size) of a multiset of objects of $k$ different types with finite repetition numbers $n_{1}, n_{2}, \ldots, n_{k}$, respectively.
--/
-
 import Combinatorics.MyMultiset.Comb
-
-namespace id_73_problem8
 
 variable {k : ℕ} (n : Fin k → ℕ)
 
@@ -13,9 +7,9 @@ def S : MyMultiset (Fin k) where
 
 instance : (S n).RepIsFinite where
   rep_finite i := by simp [S]
-
-example :
+/--
+Determine the total number of combinations (of any size) of a multiset of objects of $k$ different types with finite repetition numbers $n_{1}, n_{2}, \ldots, n_{k}$, respectively.
+-/
+theorem brualdi_ch2_36 :
     ∑ i ∈ Finset.range ((S n).card + 1), Fintype.card ((S n).Comb i) = ∑ i : Fin k, (n i + 1) := by
   sorry
-
-end id_73_problem8
--- CombiBench/brualdi_ch2_6.lean
+++ CombiBench/brualdi_ch2_6.lean
@@ -1,14 +1,7 @@
-/-
+import Mathlib
+
+/--
 How many integers greater than 5400 have both of the following properties? (a) The digits are distinct. (b) The digits 2 and 7 do not occur.
 -/
-
-import Mathlib.Data.Finset.Basic
-import Mathlib.Data.Nat.Digits
-
-namespace id_43_problem7
-
-example (s : Finset ℕ)
-    (hsa : ∀ n ∈ s, (Nat.digits 10 n).Nodup)
+theorem brualdi_ch2_6 (s : Finset ℕ) (hsa : ∀ n ∈ s, (Nat.digits 10 n).Nodup)
     (hsb : ∀ n ∈ s, 2 ∉ (Nat.digits 10 n) ∧ 7 ∉ (Nat.digits 10 n)) : s.card = 94830 := by sorry
-
-end id_43_problem7
