2843e41db8f4cc3601ea2a5f33f1edcb083c4b7f
97l <130048468+ahhwuhu@users.noreply.github.com>
2025-02-12 11:17:47 +0800

    Update easy_6.lean

--- CombiBench/easy_6.lean
+++ CombiBench/easy_6.lean
@@ -1,9 +1,7 @@
 import Mathlib.Probability.ProbabilityMassFunction.Binomial
 
 /--
-Two coins are tossed simultaneously. What is the probability of getting
-(i) At least one head? (ii) At most one tail?
-(iii) A head and a tail?
+Two coins are tossed simultaneously. What is the probability of getting (i) At least one head? (ii) At most one tail? (iii) A head and a tail?
 -/
 theorem easy_6_1: PMF.binomial (1/2 : _) ENNReal.half_le_self 2 1 +
     PMF.binomial (1/2 : _) ENNReal.half_le_self 2 2 = 3/4 := by
