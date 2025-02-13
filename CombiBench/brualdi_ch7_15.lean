c043807877822042bea6a8142cae5fcfd4c871f6
97l <130048468+ahhwuhu@users.noreply.github.com>
2025-02-13 14:31:39 +0800

    fix brualdi 22 24 and 25 (#82)

--- CombiBench/brualdi_ch7_15.lean
+++ CombiBench/brualdi_ch7_15.lean
@@ -1,9 +1,8 @@
 import Mathlib
 
-namespace brualdi_24
 /--
 Determine the generating function for the sequence of cubes \[ 0, 1, 8, \ldots, n^{3}, \ldots \]
 -/
-theorem brualdi_24 : PowerSeries.mk (fun (n : ℕ) => (n : ℝ) ^ 3) = PowerSeries.X *
+theorem brualdi_ch7_15 : PowerSeries.mk (fun (n : ℕ) => (n : ℝ) ^ 3) = PowerSeries.X *
     (PowerSeries.X ^ 2 + 4 * PowerSeries.X + 1) * PowerSeries.inv (1 - PowerSeries.X) ^ 4 := by
   sorry
