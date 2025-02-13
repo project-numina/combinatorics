678d17ec2273c068829c82c656f19052c1a3b0f8
Jujian Zhang <jujian.zhang1998@outlook.com>
2025-02-13 04:31:39 +0000

    Update brualdi_ch2_36.lean

--- CombiBench/brualdi_ch2_36.lean
+++ CombiBench/brualdi_ch2_36.lean
@@ -7,4 +7,4 @@ theorem brualdi_ch2_36 {k : ℕ} (n : Fin k → ℕ)
     (sols : Finset (Fin k → ℕ))
     (h_sols : ∀ f, f ∈ sols ↔ (∀ i, f i ≤ n i)) :
     sols.card =
-    ∑ i : Fin k, (n i + 1) := by
+    ∑ i : Fin k, (n i + 1) := by sorry
