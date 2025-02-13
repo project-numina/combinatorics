567b50d27e816e2df086036ddab6e6e3b79c5985
97l <130048468+ahhwuhu@users.noreply.github.com>
2025-02-13 17:33:48 +0800

    Update imosl_2019_c2.lean

--- CombiBench/imosl_2019_c2.lean
+++ CombiBench/imosl_2019_c2.lean
@@ -1,7 +1,5 @@
 import Mathlib
 
-set_option autoImplicit false
-
 /--
 You are given a set of $n$ blocks, each weighing at least 1; their total weight is $2 n$. Prove that for every real number $r$ with $0 \\leqslant r \\leqslant 2 n-2$ you can choose a subset of the blocks whose total weight is at least $r$ but at most $r+2$.
 -/
