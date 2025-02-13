7581e3e084baf7e19df3cd81fe549c401343a722
ahhwuhu <liujunqi@amss.ac.cn>
2025-02-13 17:48:50 +0800

    simple fix

--- CombiBench/imosl_2021_c5.lean
+++ CombiBench/imosl_2021_c5.lean
@@ -1,13 +1,7 @@
 import Mathlib
 
-/- 727
-Let $n$ and $k$ be two integers with $n > k \\geqslant 1$. There are $2n+1$ students standing in a circle. Each student $S$ has $2k$ neighbours - namely, the $k$ students closest to $S$ on the right, and the $k$ students closest to $S$ on the left. Suppose that $n+1$ of the students are girls, and the other $n$ are boys. Prove that there is a girl with at least $k$ girls among her neighbours.
--/
-
 variable {n : ℕ+} (k : ℕ+)
 variable (people : Fin (2*n+1) → ZMod 2) -- 0 for boys and 1 for girls
-variable (num_boys : (List.ofFn people).count 0 = n)
-variable (num_girls : (List.ofFn people).count 1 = n+1)
 
 def leftNeighbors (i : Fin (2*n+1)) : Fin k → ZMod 2 :=
     fun j ↦ people <| (finRotate (2*n+1))^[j.1] i
@@ -15,7 +9,11 @@ def leftNeighbors (i : Fin (2*n+1)) : Fin k → ZMod 2 :=
 def rightNeighbors (i : Fin (2*n+1)) : Fin k → ZMod 2 :=
     fun j ↦ people <| (finRotate (2*n+1)).symm^[j.1] i
 
-theorem imosl_2021_c5 (h : k < n) :
+/--
+Let $n$ and $k$ be two integers with $n > k \\geqslant 1$. There are $2n+1$ students standing in a circle. Each student $S$ has $2k$ neighbours - namely, the $k$ students closest to $S$ on the right, and the $k$ students closest to $S$ on the left. Suppose that $n+1$ of the students are girls, and the other $n$ are boys. Prove that there is a girl with at least $k$ girls among her neighbours.
+-/
+theorem imosl_2021_c5 (h : k < n) (num_boys : (List.ofFn people).count 0 = n)
+   (num_girls : (List.ofFn people).count 1 = n+1):
     ∃ (i : Fin (2*n+1)),
         people i = 1 -- this person is a girl
       ∧ ((List.ofFn (leftNeighbors k people i)).count 1 +
