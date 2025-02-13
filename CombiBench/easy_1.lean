948fbc88ea87cc92021c4c7b03ffbb6fa7188f2c
ahhwuhu <liujunqi@amss.ac.cn>
2025-02-13 20:57:07 +0800

    add1278

--- CombiBench/easy_1.lean
+++ CombiBench/easy_1.lean
@@ -1,8 +1,9 @@
-import Mathlib.Data.Fintype.Basic
-import Combinatorics.PermutationsCombinations.Combinations
+import Mathlib
 
 /--
 How many ways can a teacher select a group of 6 students to sit in the front row if the class has 13 students?
 -/
-theorem easy_1 : (combinations 6 (@Finset.univ (Fin 13) _)).card = 1716 := by
-  rw [← combinations_card]; rfl
+theorem easy_1 (sols : Finset (Fin 13 → Fin 2))
+    (h_sols : ∀ f, f ∈ sols ↔ ((@Finset.univ (Fin 13)).filter (fun i => f i = 0)).card = 6) :
+    sols.card = 1716 := by
+  sorry
--- CombiBench/easy_2.lean
+++ CombiBench/easy_2.lean
@@ -1,13 +1,12 @@
-import Mathlib.Data.Fintype.Basic
-import Mathlib.Data.Fintype.Card
-import Combinatorics.PermutationsCombinations.Permutations
-
-open Finset
+import Mathlib
 
 /--
 There are 8 athletes participating in a sprint competition. The referee needs to select 3 athletes and assign them specific rankings (first place, second place, and third place). How many different arrangements are possible?
 -/
-theorem easy_2 : (permutationsLength 3 (@univ (Fin 8) _)).card = 336 := by
-  rw [permutationsLength_card]
-  . rfl
-  . simp
+theorem easy_2 (sols : Finset (Fin 8 → Fin 4))
+    (h_sols : ∀ f, f ∈ sols ↔
+    (((@Finset.univ (Fin 8)).filter (fun i => f i = 0)).card = 1) ∧
+    (((@Finset.univ (Fin 8)).filter (fun i => f i = 1)).card = 1) ∧
+    (((@Finset.univ (Fin 8)).filter (fun i => f i = 2)).card = 1)) :
+    sols.card = 336 := by
+  sorry
