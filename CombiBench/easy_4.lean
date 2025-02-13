eeef3a6fcd5cc622c4f411a46449d4d24785204e
97l <130048468+ahhwuhu@users.noreply.github.com>
2025-02-13 22:48:00 +0800

    Update easy_4.lean

--- CombiBench/easy_4.lean
+++ CombiBench/easy_4.lean
@@ -5,6 +5,6 @@ A restaurant’s menu has 3 appetizers, 3 mains and 2 desserts. In how many way 
 -/
 theorem easy_4
     (sols : Finset (Fin 3 → ℕ))
-    (h_sols : f ∈ sols ↔ (f 0 ≤ 3 ∧ f 1 ≤ 3 ∧ f 2 ≤ 2 ∧ f 0 + f 1 + f 2 = 3)) :
+    (h_sols : ∀ f, f ∈ sols ↔ (f 0 ≤ 3 ∧ f 1 ≤ 3 ∧ f 2 ≤ 2 ∧ f 0 + f 1 + f 2 = 3)) :
     sols.card = 18 := by
     sorry
