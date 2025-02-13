fd38236ad7fc65a50972899646ddcf05aeca7e77
ahhwuhu <liujunqi@amss.ac.cn>
2025-02-13 14:17:22 +0800

    fix metadata

--- CombiBench/brualdi_ch5_51.lean
+++ CombiBench/brualdi_ch5_51.lean
@@ -1,14 +1,9 @@
 import Mathlib
 
 /--
-Let $R$ and $S$ be two partial orders on the same set $X$. Considering $R$ and $S$ as subsets of $X \times X$, we assume that $R \subseteq S$ but $R \neq S$. Show that there exists an ordered pair $(p, q)$, where $(p, q) \in S$ and $(p, q) \notin R$ such that $R^{\prime}=R \cup\{(p, q)\}$ is also a partial order on $X$. Show by example that not every $\operatorname{such}(p, q)$ has the property that $R^{\prime}$ is a partial order on $X$.
+Let $R$ and $S$ be two partial orders on the same set $X$. Considering $R$ and $S$ as subsets of $X \times X$, we assume that $R \subseteq S$ but $R \neq S$. Show that there exists an ordered pair $(p, q)$, where $(p, q) \in S$ and $(p, q) \notin R$ such that $R^{\prime}=R \cup\{(p, q)\}$ is also a partial order on $X$.
 -/
-theorem brualdi_ch5_51_1 {X : Type} [DecidableEq X] (R S : Rel X X)
+theorem brualdi_ch5_51 {X : Type} [DecidableEq X] (R S : Rel X X)
     [IsPartialOrder X R] [IsPartialOrder X S] (le : R < S) :
   ∃ (p q : X), S p q ∧ ¬ R p q ∧
   IsPartialOrder X (R ⊔ fun x y ↦ if x = p ∧ y = q then true else false) := by sorry
-
-theorem brualdi_ch5_51_2 {X : Type} [DecidableEq X] :
-    ∃ (R S : Rel X X) (_ : IsPartialOrder X R) (_ : IsPartialOrder X S)
-      (_ : R < S) (p q : X) (_ : S p q) (_ : ¬ R p q),
-      ¬ IsPartialOrder X (R ⊔ fun x y ↦ if x = p ∧ y = q then true else false) := by sorry
--- CombiBench/brualdi_ch5_9.lean
+++ CombiBench/brualdi_ch5_9.lean
@@ -3,5 +3,4 @@ import Mathlib
 /--
 Evaluate the sum $\sum_{k=0}^{n}(-1)^{k}\binom{n}{k} 10^{k}$.
 -/
-theorem brualdi_ch5_9 : ∑ i in Finset.range (n + 1), (-1 : ℤ)^i * (n.choose i) * 10^i = (-9 : ℤ)^n := by sorry
-
+theorem brualdi_ch5_9 : ∑ i ∈ Finset.range (n + 1), (-1 : ℤ) ^ i * (n.choose i) * 10 ^ i = (-9 : ℤ)^n := by sorry
