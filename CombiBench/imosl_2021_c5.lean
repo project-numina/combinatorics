aa03883e23a0bba9a4c48bdec19b2fe06d951e0c
zjj <zjj.zjj@zjj.zjj>
2025-02-13 05:13:56 +0000

    initial

--- CombiBench/imosl_2021_c5.lean
+++ CombiBench/imosl_2021_c5.lean
@@ -1,7 +1,24 @@
 import Mathlib
 
-/-- 727
+/- 727
 Let $n$ and $k$ be two integers with $n > k \\geqslant 1$. There are $2n+1$ students standing in a circle. Each student $S$ has $2k$ neighbours - namely, the $k$ students closest to $S$ on the right, and the $k$ students closest to $S$ on the left. Suppose that $n+1$ of the students are girls, and the other $n$ are boys. Prove that there is a girl with at least $k$ girls among her neighbours.
 -/
-theorem imosl_2021_c5 : 1 = 1
-    := by sorry
+
+variable {n : ℕ+} (k : ℕ+)
+variable (people : Fin (2*n+1) → ZMod 2) -- 0 for boys and 1 for girls
+variable (num_boys : (List.ofFn people).count 0 = n)
+variable (num_girls : (List.ofFn people).count 1 = n+1)
+
+def leftNeighbors (i : Fin (2*n+1)) : Fin k → ZMod 2 :=
+    fun j ↦ people <| (finRotate (2*n+1))^[j.1] i
+
+def rightNeighbors (i : Fin (2*n+1)) : Fin k → ZMod 2 :=
+    fun j ↦ people <| (finRotate (2*n+1)).symm^[j.1] i
+
+theorem imosl_2021_c5 (h : k < n) :
+    ∃ (i : Fin (2*n+1)),
+        people i = 1 -- this person is a girl
+      ∧ ((List.ofFn (leftNeighbors k people i)).count 1 +
+         (List.ofFn (rightNeighbors k people i)).count 1 >= k)
+        -- number of girls among the neighbours is at least k
+          := by sorry
