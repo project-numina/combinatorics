e41a4ad659bdf8f6a6d7b2e697d080753ba4cf85
zjj <zjj.zjj@zjj.zjj>
2025-02-13 14:09:27 +0000

    easy_3 and easy_4

--- CombiBench/easy_3.lean
+++ CombiBench/easy_3.lean
@@ -3,5 +3,4 @@ import Mathlib
 /--
 An ice cream machine has 3 flavors - vanilla, chocolate, and strawberry. The ice cream can be served in 2 ways - in a cone or in a cup. Along with the ice cream, there are 5 options for toppings - hot fudge, caramel, nuts, cherries, and sprinkles.
 -/
-theorem easy_3 : 1 = 1 := by
-    sorry
+theorem easy_3 : Fintype.card (Fin 3 × Fin 2 × Fin 5) = 30 := by sorry
--- CombiBench/easy_4.lean
+++ CombiBench/easy_4.lean
@@ -3,5 +3,8 @@ import Mathlib
 /--
 A restaurant’s menu has 3 appetizers, 3 mains and 2 desserts. In how many way can a 3-course meal be ordered?
 -/
-theorem easy_4 : 1 = 1 := by
+theorem easy_4
+    (sols : Finset (Fin 3 → ℕ))
+    (h_sols : f ∈ sols ↔ (f 0 ≤ 3 ∧ f 1 ≤ 3 ∧ f 2 ≤ 2 ∧ f 0 + f 1 + f 2 = 3)) :
+    sols.card = 18 := by
     sorry
