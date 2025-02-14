16365b4cedbad4f9c49ce9cae19b2117134a20ad
ahhwuhu <liujunqi@amss.ac.cn>
2025-02-14 16:18:04 +0800

    alignment

--- CombiBench/easy_5.lean
+++ CombiBench/easy_5.lean
@@ -1,12 +1,7 @@
 import Mathlib
 
-open MeasureTheory ProbabilityTheory ENNReal
 /--
 There are 10 red marbles, 6 green marbles, and 4 blue marbles in a box. What is the probability of picking the next red marble?
 -/
-theorem easy_5 : uniformOn (Ω := (Fin 20)) ⊤ {i | i.1 < 10} = 1/2 := by
-  erw [uniformOn_univ]
-  rw [Measure.count_apply_finite' (Set.toFinite _) (by trivial), Set.Finite.card_toFinset,
-    show Fintype.card (@Set.Elem (Fin 20) {(i : Fin 20) | i.1 < 10}) = 10 by rfl]
-  simp only [Nat.cast_ofNat, Fintype.card_fin]
-  rw [ENNReal.div_eq_div_iff] <;> simp; norm_num
+theorem easy_5 : 1 = 1 := by
+    sorry
--- CombiBench/easy_6.lean
+++ CombiBench/easy_6.lean
@@ -3,9 +3,5 @@ import Mathlib
 /--
 Two coins are tossed simultaneously. What is the probability of getting (i) At least one head? (ii) At most one tail? (iii) A head and a tail?
 -/
-theorem easy_6: PMF.binomial (1/2 : _) ENNReal.half_le_self 2 1 +
-    PMF.binomial (1/2 : _) ENNReal.half_le_self 2 2 = 3/4 ∧
-    PMF.binomial (1/2 : _) ENNReal.half_le_self 2 0 +
-    PMF.binomial (1/2 : _) ENNReal.half_le_self 2 1 = 3/4 ∧
-    PMF.binomial (1/2 : _) ENNReal.half_le_self 2 1 = 1/2 := by
-  sorry
+theorem easy_6 : 1 = 1 := by
+    sorry
