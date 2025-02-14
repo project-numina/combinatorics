edc8a4689e05d014597b32baad5cbe3c7295af9f
Jujian Zhang <jujian.zhang1998@outlook.com>
2025-02-14 12:36:26 +0000

    Update easy_4.lean

--- CombiBench/easy_4.lean
+++ CombiBench/easy_4.lean
@@ -3,7 +3,5 @@ import Mathlib
 /--
 A restaurant’s menu has 3 appetizers, 3 mains and 2 desserts. In how many way can a 3-course meal be ordered?
 -/
-theorem easy_4
-    (sols : Finset (Fin 3 → ℕ))
-    (h_sols : ∀ f, f ∈ sols ↔ (f 0 ≤ 3 ∧ f 1 ≤ 3 ∧ f 2 ≤ 2 ∧ f 0 + f 1 + f 2 = 3)) :
-    sols.card = 18 := by sorry
+theorem easy_4 :
+    Fintype.card (Fin 3 × Fin 3 × Fin 2) = 18 := by sorry
