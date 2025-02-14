560d3a21dafbe42590f6b1d2de5cda35645c1e96
ahhwuhu <liujunqi@amss.ac.cn>
2025-02-14 16:41:23 +0800

    some change of two prb

--- CombiBench/brualdi_ch10_34.lean
+++ CombiBench/brualdi_ch10_34.lean
@@ -1,15 +1,12 @@
 import Mathlib
 
-structure SteinerTripleSystem (t k n : ℕ) (α : Type u) where
-  carrier : Finset α
-  card_eq_n : carrier.card = n
-  blocks : Finset (Finset α)
-  blocks_subset : ∀ b ∈ blocks, b ⊆ carrier
-  card_blocks : blocks.card = k
-  block_inner : ∀ s : (Finset α), s.card = t ∧ s ⊆ carrier →
-    ∃! b ∈ blocks, s ⊆ b
+structure SteinerTripleSystem (t k n : ℕ) where
+  carrier : Fin n
+  blocks : Finset (Finset (Fin n))
+  card_blocks : ∀ b ∈ blocks, b.card = k
+  block_inner : ∀ s : (Finset (Fin n)), s.card = t → ∃! b ∈ blocks, s ⊆ b
 /--
 Let $t$ be a positive integer. Prove that, if there exists a Steiner triple system of index 1 having $v$ varieties, then there exists a Steiner triple system having $v^{t}$ varieties.
 -/
-theorem brualdi_ch10_34 (t : ℕ+) (v : ℕ): Nonempty (SteinerTripleSystem v 2 3 α) →
-  Nonempty (SteinerTripleSystem (v^t.1) 2 3 α) := sorry
+theorem brualdi_ch10_34 (t : ℕ+) (v : ℕ): Nonempty (SteinerTripleSystem 2 3 v) →
+  Nonempty (SteinerTripleSystem 2 3 (v^t.1)) := sorry
--- CombiBench/brualdi_ch10_59.lean
+++ CombiBench/brualdi_ch10_59.lean
@@ -4,12 +4,6 @@ structure LatinSquare (n : ℕ) where
   carrier : Matrix (Fin n) (Fin n) (ZMod n)
   pairwise_1 : ∀ i j1 j2, j1 ≠ j2 → carrier i j1 ≠ carrier i j2
   pairwise_2 : ∀ j i1 i2, i1 ≠ i2 → carrier i1 j ≠ carrier i2 j
-
-instance : CoeSort (LatinSquare n) (Type) where
-  coe _ := Matrix (Fin n) (Fin n) (ZMod n)
-
-lemma IsIdem_of_diag_sorted: ∀ (n : ℕ) (L : LatinSquare n),
-  (∀ i, L.1 i i = i.1) → IsIdempotentElem L.1 := sorry
 /--
 A Latin square of order $n$ (based on $Z_{n}$) is idempotent, provided that its entries on the diagonal running from upper left to lower right are $0,1,2,\ldots,n-1$.\\ (1) Construct an example of an idempotent Latin square of order 5.\\ (2) Construct an example of a symmetric, idempotent Latin square of order 5.
 -/
