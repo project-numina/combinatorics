4a916658bce936edcf5474f047d2286180694b5e
xbb <xbbng.0@gmail.com>
2025-02-14 20:08:05 +0800

    brualdi12 fix

--- problem11_15/id_165_problem_12.lean
+++ problem11_15/id_165_problem_12.lean
@@ -1,18 +1,17 @@
-import Combinatorics.PermutationsCombinations.Combinations
 import Mathlib
-
-open Nat Finset Set
 
 variable {α : Type*} {n r : ℕ}
 
-def A (n : ℕ) := Finset.Icc 1 n  
+
+def A (n : ℕ) := Finset.Icc 1 n
+
 
 -- Define the complement
 def complement (n : ℕ) (a : Finset ℕ) : Finset ℕ :=
   (A n).filter (λ x => x ∉ a)
 
 -- Convert to lexicographic order.
-noncomputable def to_list_a' {n r : ℕ} := (Finset.toList (combinations r (A n))).reverse
+noncomputable def to_list_a' {n r : ℕ} := (Finset.toList ((A n).powersetCard r)).reverse
 
 -- Lexicographic order of the complement
 noncomputable def to_list_b (n : ℕ) (a : Finset ℕ ) := (Finset.toList (complement n a)).reverse
@@ -25,6 +24,5 @@ at the same time, the number of $(n-r)$ subsets of $\{1,2, \ldots, n\}$. Prove t
 A_{M}$ are the $r$-subsets in lexicographic order, then $\overline{A_{M}}, \ldots, \overline{A_{3}}, \overline{A_{2}},
 \overline{A_{1}}$ are the $(n-r)$-subsets in lexicographic order.
 -/
-theorem id_165 (r n M: ℕ) (A₀ : List (Finset ℕ ) ) (hA₀ : A₀.length = (combinations r (A n)).card) (hM : M = A₀.length ) :
-  if A₀ = ((Finset.toList (combinations r (A n))).reverse)
-  then  (A₀.map (complement n)).reverse =  (Finset.toList (combinations (n - r) (A n))).reverse  else False := by sorry
+theorem id_165 (r n M: ℕ) (A₀ : List (Finset ℕ) ) (hA₀ : A₀.length = ((A n).powersetCard r).card) (hM : M = A₀.length ) :
+  A₀ = ((Finset.toList ((A n).powersetCard r)).reverse) → (A₀.map (complement n)).reverse =  (Finset.toList ((A n).powersetCard (n - r))).reverse := by sorry
