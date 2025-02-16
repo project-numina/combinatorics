6f31b84879e67f6db29e2937f8c8c02bab59ab63
zjj <zjj.zjj@zjj.zjj>
2025-02-16 05:31:50 +0000

    imo_2024_p3

--- CombiBench/imo_2024_p3.lean
+++ CombiBench/imo_2024_p3.lean
@@ -1,24 +1,7 @@
 import Mathlib
 
-def Count (l : List ℕ+) (n : ℕ) : ℕ :=
-  match l with
-  | []      => 0
-  | x :: xs => (if x == n then 1 else 0) + Count xs n
-
-def buildSequence (initSeq : List ℕ+) (h : initSeq ≠ []) : ℕ → ℕ+ := fun n ↦
-  let initLen := initSeq.length
-  if n <= initLen then
-    ⟨initSeq.get! (n - 1), PNat.pos _⟩
-  else
-    let steps := n - initLen
-    let rec extend (k : Nat) (seq : List ℕ+) : Nat :=
-      match k with
-      | 0 => seq.getLast?.getD 1  -- If no more steps, return the last term
-      | k+1 =>
-          let lastElem := seq.getLast?.getD 1
-          let newElem : ℕ+ := ⟨Count seq lastElem, by sorry⟩
-          extend k (seq ++ [newElem])
-    ⟨extend steps initSeq, by sorry⟩
+def isEventuallyPeriodic (b : ℕ → ℕ) : Prop :=
+  ∃ p M, ∀ m, M ≤ m → b (m + p) = b m
 
 /--
 Let $a_1, a_2, a_3, \dots$ be an infinite sequence of positive integers,
@@ -29,5 +12,7 @@ $a_1, a_3, a_5, \dots$ and $a_2, a_4, a_6, \dots$ is eventually periodic.
 (An infinite sequence $b_1, b_2, b_3, \dots$ is eventually periodic if
 there exist positive integers $p$ and $M$ such that $b_{m+p} = b_m$ for all $m \ge M$.)
 -/
-theorem imo_2024_p3 : 1 = 1
-    := by sorry
+theorem imo_2024_p3
+  (a : ℕ → ℕ) (ha : ∀ n, 0 < a n) (N : ℕ)
+  (h : ∀ n > N, a n = List.count (a (n - 1)) (List.ofFn (a ∘ Fin.val (n := n -1)))) :
+  (isEventuallyPeriodic (a ∘ (2 * ·))) ∨ (isEventuallyPeriodic (a ∘ (2 * · + 1))) := by sorry
