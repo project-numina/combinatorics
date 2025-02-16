69f7ff579d4feb26cd29bfd4a5cc67180bee1231
zjj <zjj.zjj@zjj.zjj>
2025-02-16 04:50:14 +0000

    fix

--- CombiBench/easy_9.lean
+++ CombiBench/easy_9.lean
@@ -3,6 +3,7 @@ import Mathlib
 namespace easy_9
 open Finset
 
+-- #time
 -- #eval (Finset.filter (fun f : Fin 6 → Fin 11 => (∀ i, f i > 0) ∧ ∑ i, (f i : ℕ) = 10) (@Finset.univ (Fin 6 → Fin 11) _)).card, indeed 126
 
 /--
