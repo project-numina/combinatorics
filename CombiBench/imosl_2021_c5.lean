5c8d2b3bbb586743a25cf8b1a8befc0975ffa6eb
Jujian Zhang <jujian.zhang1998@outlook.com>
2025-02-17 03:22:19 +0000

    fix imosl_2021_c5 (#90)
    * fix
    
    * del #time
    
    ---------
    
    Co-authored-by: zjj <zjj.zjj@zjj.zjj>
    Co-authored-by: ahhwuhu <liujunqi@amss.ac.cn>

--- CombiBench/imosl_2021_c5.lean
+++ CombiBench/imosl_2021_c5.lean
@@ -2,10 +2,10 @@ import Mathlib
 
 
 def leftNeighbors {n : ℕ+} (people : Fin (2*n+1) → ZMod 2) (k : ℕ+) (i : Fin (2*n+1)) : Fin k → ZMod 2 :=
-    fun j ↦ people <| (finRotate (2*n+1))^[j.1] i
+    fun j ↦ people <| (finRotate (2*n+1))^[j.1 + 1] i
 
 def rightNeighbors {n : ℕ+} (people : Fin (2*n+1) → ZMod 2) (k : ℕ+) (i : Fin (2*n+1)) : Fin k → ZMod 2 :=
-    fun j ↦ people <| (finRotate (2*n+1)).symm^[j.1] i
+    fun j ↦ people <| (finRotate (2*n+1)).symm^[j.1 + 1] i
 
 /--
 Let $n$ and $k$ be two integers with $n > k \\geqslant 1$. There are $2n+1$ students standing in a circle. Each student $S$ has $2k$ neighbours - namely, the $k$ students closest to $S$ on the right, and the $k$ students closest to $S$ on the left. Suppose that $n+1$ of the students are girls, and the other $n$ are boys. Prove that there is a girl with at least $k$ girls among her neighbours.
