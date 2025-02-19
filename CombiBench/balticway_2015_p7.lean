91d5e0c23ecd8c44755632a37f92ba4852ec1b68
Jonas Bayer <jonas.bayer999@gmail.com>
2025-02-19 08:33:44 +0000

    balticway_2015_p7 (ladies problem) (#97)
    * Statement for ladies problem
    
    * Remove unnecessary instance statement

--- CombiBench/balticway_2015_p7.lean
+++ CombiBench/balticway_2015_p7.lean
@@ -1,7 +1,17 @@
 import Mathlib
 
+open SimpleGraph Finset
+
+abbrev Ladies := Fin 100
+
 /-- 135
 There are 100 members in a ladies' club. Each lady has had tea (in private) with exactly 56 of the other members of the club. The Board, consisting of the 50 most distinguished ladies, have all had tea with one another. Prove that the entire club may be split into two groups in such a way that, within each group, any lady has had tea with any other.
 -/
-theorem balticway_2015_p7 : 1 = 1
-    := by sorry
+theorem balticway_2015_p7 (had_tea: SimpleGraph (Ladies)) [DecidableRel had_tea.Adj]
+  (h_had_tea_with_56: ∀ l: Ladies, had_tea.degree l = 56)
+  (h_board: ∃ board : Finset Ladies, board.card = 50 ∧ (∀ l1 l2: board, had_tea.Adj l1 l2)) :
+  ∃ group1 group2: Finset Ladies,
+    group1 ∪ group2 = Finset.univ
+    ∧ Disjoint group1 group2
+    ∧ (∀ l1 l2: group1, had_tea.Adj l1 l2)
+    ∧ (∀ l1 l2: group2, had_tea.Adj l1 l2) := by sorry
