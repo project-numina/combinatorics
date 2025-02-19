5e66cf50873faea80b9b53cc872e1bfe016a4e8f
97l <130048468+ahhwuhu@users.noreply.github.com>
2025-02-19 15:50:32 +0800

    all comb problem since 2000 without figure (#102)
    * all comb problem since 2000 without figure
    
    * fix a tag
    
    * delete python
    
    * metadata format

---
+++ CombiBench/imo_2003_p1.lean
@@ -0,0 +1,6 @@
+import Mathlib
+
+/--
+$S$ is the set $\{1, 2, 3, \dots ,1000000\}$. Show that for any subset $A$ of $S$ with $101$ elements we can find $100$ distinct elements $x_i$ of $S$, such that the sets $\{a + x_i \mid a \in A\}$ are all pairwise disjoint.
+-/
+theorem imo_2003_p1 : 0 = 0 := by sorry
---
+++ CombiBench/imo_2005_p6.lean
@@ -0,0 +1,7 @@
+import Mathlib
+
+
+/--
+In a mathematical competition, in which 6 problems were posed to the participants, every two of these problems were solved by more than 2/5 of the contestants. Moreover, no contestant solved all the 6 problems. Show that there are at least 2 contestants who solved exactly 5 problems each.
+-/
+theorem imo_2005_p6 : 1 = 1 := by sorry
