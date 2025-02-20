5e38f1b3e3e52f3b6190c82891fa1e5619348e2c
ahhwuhu <liujunqi@amss.ac.cn>
2025-02-20 20:42:59 +0800

    remove abandon problem

--- CombiBench/brualdi_ch4_35.lean
+++ CombiBench/brualdi_ch4_35.lean
@@ -1,16 +1,6 @@
 import Mathlib
 
 open List Lex
-
--- Define the complement
-noncomputable def complement (n : ℕ) (a : List ℕ) : List ℕ := (List.Ico 1 (n+1)).filter (λ x => x ∉ a)
-
--- the r subset.
-noncomputable def to_list_A (n r : ℕ) := ((((Finset.Ico 1 (n+1)).powersetCard r)).toList).map (Finset.toList)
-
--- the reverse order of (n-r) subset
-noncomputable def to_list_B (n r : ℕ) :=  (((((Finset.Ico 1 (n+1)).powersetCard (n - r))).toList).map (Finset.toList)).reverse
-
 /--
 The complement $\bar{A}$ of an $r$-subset $A$ of $\{1,2, \ldots, n\}$ is the $(n-r)$-subset of $\{1,2, \ldots, n\}$,
 consisting of all those elements that do not belong to $A$. Let $M=\binom{n}{r}$, the number of $r$-subsets and,
@@ -18,6 +8,9 @@ at the same time, the number of $(n-r)$ subsets of $\{1,2, \ldots, n\}$. Prove t
 A_{M}$ are the $r$-subsets in lexicographic order, then $\overline{A_{M}}, \ldots, \overline{A_{3}}, \overline{A_{2}},
 \overline{A_{1}}$ are the $(n-r)$-subsets in lexicographic order.
 -/
-theorem brualdi_ch4_35 (r n M : ℕ)  (hM : M = ((Finset.Ico 1 (n+1)).powersetCard r).card) :
- ∀ x y , x ∈ to_list_A n r ∧ y ∈ to_list_A  n r → Lex (· < ·) x y →
-  complement n x ∈ to_list_B n r ∧ complement n y ∈ to_list_B n r →  Lex (· < ·) (complement n x) (complement n y) := by sorry
+theorem brualdi_ch4_35 (r n M : ℕ) (hM : M = ((@Finset.univ (Fin n)).powersetCard r).card)
+    (h : Fin M → (Finset.powersetCard r (@Finset.univ (Fin M) _))) :
+    ∀ i j, (List.Lex (fun x1 x2 : Fin M => x1 ≤ x2)
+    (Finset.sort (· ≤ ·) (h i)) (Finset.sort (· ≤ ·) (h j))) →
+    (List.Lex (fun x1 x2 : Fin M => x1 ≤ x2)
+    (Finset.sort (· ≤ ·) (h j)ᶜ) (Finset.sort (· ≤ ·) (h i)ᶜ)) := by sorry
