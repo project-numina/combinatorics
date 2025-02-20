fa087fa8d184cf5b0d96180e8d9830addbe5e42d
ahhwuhu <liujunqi@amss.ac.cn>
2025-02-20 18:31:08 +0800

    fix format

--- CombiBench/easy_5.lean
+++ CombiBench/easy_5.lean
@@ -1,7 +1,10 @@
 import Mathlib
 
+noncomputable abbrev easy_5_solution : ENNReal := sorry
+
 open MeasureTheory ProbabilityTheory ENNReal
 /--
 There are 10 red marbles, 6 green marbles, and 4 blue marbles in a box. What is the probability of picking the next red marble?
 -/
-theorem easy_5 : uniformOn (Ω := (Fin 20)) ⊤ {i | i.1 < 10} = 1/2 := by sorry
+theorem easy_5 : uniformOn (Ω := (Fin 20)) ⊤ {i | i.1 < 10} = easy_5_solution := by
+  sorry
