20a2d644209a229469445c19e2bad93f602e0732
zjj <zjj.zjj@zjj.zjj>
2025-02-24 04:15:24 +0000

    fix

--- CombiBench/imo_2022_p6.lean
+++ CombiBench/imo_2022_p6.lean
@@ -26,10 +26,10 @@ noncomputable instance {n : ℕ} (sq : NordicSquare n) : Fintype (UphillPath sq)
 /-- 751
 Let $n$ be a positive integer. A Nordic square is an $n \times n$ board containing all the integers from $1$ to $n^2$ so that each cell contains exactly one number. Two different cells are considered adjacent if they share an edge. Every cell that is adjacent only to cells containing larger numbers is called a valley. An uphill path is a sequence of one or more cells such that: (i) the first cell in the sequence is a valley, (ii) each subsequent cell in the sequence is adjacent to the previous cell, and (iii) the numbers written in the cells in the sequence are in increasing order. Find, as a function of $n$, the smallest possible total number of uphill paths in a Nordic square.
 -/
-def minNumUphillPaths : ℕ → ℕ := sorry
+def imo_2022_p6 : ℕ → ℕ := sorry
 
-lemma imo_2022_p6 (n : ℕ) :
-  ∃ (sq : NordicSquare n), minNumUphillPaths n = Fintype.card (UphillPath sq) := sorry
+lemma imo_2022_p6_cond₀ (n : ℕ) :
+  ∃ (sq : NordicSquare n), imo_2022_p6 n = Fintype.card (UphillPath sq) := sorry
 
 lemma imo_2022_p6_cond₁ (n : ℕ) :
-  ∀ (sq : NordicSquare n), minNumUphillPaths n ≤ Fintype.card (UphillPath sq) := sorry
+  ∀ (sq : NordicSquare n), imo_2022_p6 n ≤ Fintype.card (UphillPath sq) := sorry
