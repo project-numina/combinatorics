a5c179bc055d3e128f6fe4026710d2cd511e8cd8
Jonas Bayer <jonas.bayer999@gmail.com>
2025-02-25 12:54:34 +0000

    Style: Use factorial notation

--- CombiBench/imo_2001_p4.lean
+++ CombiBench/imo_2001_p4.lean
@@ -1,5 +1,7 @@
 import Mathlib
 
+open Nat
+
 def f {m : ℕ} (n : Finset.Icc 1 m → ℤ) (x : Equiv.Perm (Finset.Icc 1 m)) : ℤ :=
   ∑ i, x i * n i
 
@@ -7,4 +9,4 @@ def f {m : ℕ} (n : Finset.Icc 1 m → ℤ) (x : Equiv.Perm (Finset.Icc 1 m)) :
 Let $n_1, n_2, \dots , n_m$ be integers where $m>1$ is odd. Let $x = (x_1, \dots , x_m)$ denote a permutation of the integers $1, 2, \cdots , m$. Let $f(x) = x_1n_1 + x_2n_2 + ... + x_mn_m$. Show that for some distinct permutations $a$, $b$ the difference $f(a) - f(b)$ is a multiple of $m!$.
 -/
 theorem imo_2001_p4 (m : ℕ) (h_m_pos: m > 1) (h_m_odd: Odd m) (n : Finset.Icc 1 m → ℤ):
-  ∃ a b : Equiv.Perm (Finset.Icc 1 m), a ≠ b ∧ ↑(Nat.factorial m) ∣ (f n a - f n b) := sorry
+  ∃ a b : Equiv.Perm (Finset.Icc 1 m), a ≠ b ∧ ↑(m !) ∣ (f n a - f n b) := sorry
