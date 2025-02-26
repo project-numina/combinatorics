9b642d2203a18081060ada91250c434f074eae98
Whysoserioushah <yx3021@ic.ac.uk>
2025-02-26 08:48:06 +0000

    Revert "finish brualdi_ch6_9"
    This reverts commit 7321f94dcb95a09c39459c1e89c22229e79c5482.

--- CombiBench/brualdi_ch6_9.lean
+++
@@ -1,7 +0,0 @@
-import Mathlib
-
-abbrev brualdi_ch6_9_solution : ℕ := sorry
-
-/-- Determine the number of integral solutions of the equation x1+x2+x3+x4=20 that satisfy 1≤x1≤6,0≤x2≤7,4≤x3≤8,2≤x4≤6.-/
-theorem brualdi_ch6_9 :
-  {x : Fin 4 → ℕ | x 0 ∈ Finset.Icc 1 6 ∧ x 1 ∈ Finset.Icc 0 7 ∧ x 2 ∈ Finset.Icc 4 8 ∧ x 3 ∈ Finset.Icc 2 6}.ncard = brualdi_ch6_9_solution := by sorry
