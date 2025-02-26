6201a4ebdf6f14526fb95b5d4604d4d2f041be2b
ahhwuhu <liujunqi@amss.ac.cn>
2025-02-26 22:15:25 +0800

    imo_2011_p4

--- CombiBench/imo_2011_p4.lean
+++ CombiBench/imo_2011_p4.lean
@@ -1,8 +1,21 @@
 import Mathlib
 
--- (2n-1)!!
+abbrev imo_2011_p4_solution (n : ℕ) : ℕ := sorry
+
+-- "σ" represents which one to place, "f" represents whether to place it on the left or the right side, with 0 being the right side and 1 being the left side, and "weights" represents the weights.
+def right_le_left {n : ℕ} (σ : Equiv.Perm (Fin n)) (f : Fin n → Fin 2) (weights : Fin n → ℕ): Prop :=
+  ∀ i, ∑ j ∈ (@Finset.univ (Fin n) _).filter (fun j => f j = 0 ∧ σ j < i), weights j ≤
+  ∑ j ∈ (@Finset.univ (Fin n) _).filter (fun j => f j = 1 ∧ σ j < i), weights j
 
 /--
 Let $n > 0$ be an integer. We are given a balance and $n$ weights of weight $2^0,2^1, \cdots ,2^{n-1}$. We are to place each of the $n$ weights on the balance, one after another, in such a way that the right pan is never heavier than the left pan. At each step we choose one of the weights that has not yet been placed on the balance, and place it on either the left pan or the right pan, until all of the weights have been placed. Determine the number of ways in which this can be done.
 -/
-theorem imo_2011_p4 : 1 = 1 := sorry
+theorem imo_2011_p4 (n : ℤ) (hn : n > 0) (weights : Fin n.toNat → ℕ)
+    (h_weights : ∀ i, weights i = 2 ^ i.1) :
+    (Finset.product (@Finset.univ (Equiv.Perm (Fin n.toNat)) _)
+    (@Finset.univ (Fin n.toNat → Fin 2) _) |>.filter
+    (fun (σ, f) =>
+    ∀ i, ∑ j ∈ (@Finset.univ (Fin n.toNat) _).filter (fun j => f j = 0 ∧ σ j < i), weights j ≤
+    ∑ j ∈ (@Finset.univ (Fin n.toNat) _).filter (fun j => f j = 1 ∧ σ j < i), weights j)).card =
+    imo_2011_p4_solution n.toNat := by
+    sorry
