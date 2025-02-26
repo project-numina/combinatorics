6c61ae3f7f9bdd020cec7b140ff4047db2d0f01e
zjj <zjj.zjj@zjj.zjj>
2025-02-26 04:53:42 +0000

    rename

--- CombiBench/brualdi_ch4_31.lean
+++ CombiBench/brualdi_ch4_31.lean
@@ -1,9 +1,9 @@
 import Mathlib
 
-def sols : Finset (Fin 3 ↪ Set.Icc 1 5) := insert sorry sorry
+def brualdi_ch4_31_sol : Finset (Fin 3 ↪ Set.Icc 1 5) := insert sorry sorry
 
 /--
 Generate the $3$-permutations of ${1,2,3,4,5}$.
 -/
 theorem brualdi_ch4_31 :
-  Finset.univ (α := Fin 3 ↪ Set.Icc 1 5) = sols := by sorry
+  Finset.univ (α := Fin 3 ↪ Set.Icc 1 5) = brualdi_ch4_31_sol := by sorry
