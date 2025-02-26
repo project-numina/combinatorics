a641bd37dce1dc1043b16dc23a96ce7542017a79
Jujian Zhang <jujian.zhang1998@outlook.com>
2025-02-26 09:46:29 +0000

    imosl_2015_c6 and imosl_2011_c6 (#91)
    * add1278
    
    * change @Finset.univ .. .filter .card to List.ofFn count
    
    * imosl_2015_c6
    
    * fix
    
    * initial
    
    * update
    
    * Update imosl_2015_c6.lean
    
    * Update imosl_2011_c6.lean
    
    * Update imosl_2011_c6.lean
    
    ---------
    
    Co-authored-by: ahhwuhu <liujunqi@amss.ac.cn>
    Co-authored-by: zjj <zjj.zjj@zjj.zjj>
    Co-authored-by: 97l <130048468+ahhwuhu@users.noreply.github.com>

--- CombiBench/imosl_2015_c6.lean
+++ CombiBench/imosl_2015_c6.lean
@@ -1,7 +1,12 @@
 import Mathlib
 
-/-- 594
+def clean (S : Set ℕ+) (n : ℕ) : Prop :=
+  ∃! (f : ℕ →₀ ℕ),
+    (∑ i ∈ f.support, f i = n) ∧
+    (Odd f.support.card) ∧
+    (∀ (i : ℕ) (h : i ∈ f.support), ⟨f i, Nat.pos_of_ne_zero <| by aesop⟩ ∈ S)
+
+/--
 Let $S$ be a nonempty set of positive integers. We say that a positive integer $n$ is clean if it has a unique representation as a sum of an odd number of distinct elements from $S$. Prove that there exist infinitely many positive integers that are not clean.
 -/
-theorem imosl_2015_c6 : 1 = 1
-    := by sorry
+theorem imosl_2015_c6 (S : Set ℕ+) : ∀ (N : ℕ), ∃ (m : ℕ), N < m ∧ ¬ clean S m := by sorry
