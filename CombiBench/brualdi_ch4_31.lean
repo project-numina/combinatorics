dbf39c9d54695900e944674ce84006ecd28a8fde
zjj <zjj.zjj@zjj.zjj>
2025-02-26 04:57:13 +0000

    better?

--- CombiBench/brualdi_ch4_31.lean
+++ CombiBench/brualdi_ch4_31.lean
@@ -1,6 +1,12 @@
 import Mathlib
 
-def brualdi_ch4_31_sol : Finset (Fin 3 ↪ Set.Icc 1 5) := insert sorry sorry
+def brualdi_ch4_31_sol : Finset (Fin 3 ↪ Set.Icc 1 5) :=
+  { sorry, sorry, sorry, sorry, sorry, sorry, sorry, sorry, sorry, sorry,
+    sorry, sorry, sorry, sorry, sorry, sorry, sorry, sorry, sorry, sorry,
+    sorry, sorry, sorry, sorry, sorry, sorry, sorry, sorry, sorry, sorry,
+    sorry, sorry, sorry, sorry, sorry, sorry, sorry, sorry, sorry, sorry,
+    sorry, sorry, sorry, sorry, sorry, sorry, sorry, sorry, sorry, sorry,
+    sorry, sorry, sorry, sorry, sorry, sorry, sorry, sorry, sorry, sorry }
 
 /--
 Generate the $3$-permutations of ${1,2,3,4,5}$.
