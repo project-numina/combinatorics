3a8e935e137551d289ab905876d68c2dbc046745
zjj <zjj.zjj@zjj.zjj>
2025-02-26 04:40:04 +0000

    init

---
+++ CombiBench/brualdi_ch3_4.lean
@@ -0,0 +1,7 @@
+import Mathlib
+
+/--
+Show that if n+1 integers are chosen from the set 1,2,…,2n, then there are always two which differ by 1.
+-/
+theorem brualdi_ch3_4 (n : ℕ) (S : Finset ℕ) (elem_range : ∀ s ∈ S, (1 ≤ s ∧ s ≤ 2 * n))
+    (card : S.card = n + 1) : ∃ s ∈ S, ∃ s' ∈ S, s = s' + 1 := by sorry
