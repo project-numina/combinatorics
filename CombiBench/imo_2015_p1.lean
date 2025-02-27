05fae586e1357752a98e85a596ef2fe8c4903efe
zjj <zjj.zjj@zjj.zjj>
2025-02-27 23:06:25 +0000

    fix

--- CombiBench/imo_2015_p1.lean
+++ CombiBench/imo_2015_p1.lean
@@ -9,9 +9,11 @@ def centre_free (S : Set (ℝ × ℝ)) : Prop :=
   ∀ A ∈ S, ∀ B ∈ S, ∀ C ∈ S, A ≠ B → B ≠ C → A ≠ C →
     ¬ (∃ P ∈ S, dist A P = dist B P ∧ dist B P = dist C P)
 
+def imo_2015_sol : ℕ → Prop := sorry
+
 /--
 We say that a finite set $\mathcal{S}$ in the plane is balanced if, for any two different points $A$, $B$ in $\mathcal{S}$, there is a point $C$ in $\mathcal{S}$ such that $AC=BC$. We say that $\mathcal{S}$ is centre-free if for any three points $A$, $B$, $C$ in $\mathcal{S}$, there is no point $P$ in $\mathcal{S}$ such that $PA=PB=PC$. (1) Show that for all integers $n\geq 3$, there exists a balanced set consisting of $n$ points. (1) Determine all integers $n\geq 3$ for which there exists a balanced centre-free set consisting of $n$ points.
 -/
 theorem imo_2015_p1 :
   (∀ n ≥ 3, ∃ (S : Finset (ℝ × ℝ)), balanced S ∧ S.card = n) ∧
-  (∀ n ≥ 3, (∃ (S : Finset (ℝ × ℝ)), balanced S ∧ centre_free S ∧ S.card = n) ↔ Odd n) := by sorry
+  (∀ n ≥ 3, (∃ (S : Finset (ℝ × ℝ)), balanced S ∧ centre_free S ∧ S.card = n) ↔ imo_2015_sol n) := by sorry
