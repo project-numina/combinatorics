e52c584a5767a8aada0071630742d5194b0df491
97l <130048468+ahhwuhu@users.noreply.github.com>
2025-03-03 15:32:50 +0800

    Update brualdi_ch7_7.lean

--- CombiBench/brualdi_ch7_7.lean
+++ CombiBench/brualdi_ch7_7.lean
@@ -1,5 +1,7 @@
 import Mathlib
 
-/--Let m and n be positive integers whose greatest common divisor is d. Prove that the greatest common divisor of the Fibonacci numbers fm and fn is the Fibonacci number fd.-/
+/--
+Let $m$ and $n$ be positive integers whose greatest common divisor is $d$. Prove that the greatest common divisor of the Fibonacci numbers $f_{m}$ and $f_{n}$ is the Fibonacci number $f_{d}$.
+-/
 theorem brualdi_ch7_7 (m n d : ℕ+) (hmd : d = Nat.gcd m n) :
     Nat.gcd (Nat.fib m) (Nat.fib n) = Nat.fib d := by sorry
