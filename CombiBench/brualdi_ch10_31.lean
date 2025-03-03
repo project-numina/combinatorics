0e47b63c8feebd449bd4abf6548a563b1c66f58c
97l <130048468+ahhwuhu@users.noreply.github.com>
2025-03-03 18:36:35 +0800

    Update brualdi_ch10_31.lean

--- CombiBench/brualdi_ch10_31.lean
+++ CombiBench/brualdi_ch10_31.lean
@@ -1,5 +1,4 @@
-import Mathlib.Algebra.BigOperators.Group.Finset.Defs
-import Mathlib.Data.ZMod.Defs
+import Mathlib
 
 /-- A difference set in a (Add)group with order n is a subset with order k such that
   for any -/
@@ -8,7 +7,9 @@ structure isDifferenceSet (n k : ℕ) (B : Finset (ZMod n)): Prop where
   mem_appearance : ∀ x : (ZMod n), ∑ i ∈ B, ∑ j ∈ B\{i},
     List.count x [i - j] = (k * (k - 1))/(n - 1)
 
-/-- Prove that B = {0, 3, 4, 9, 11} is a difference set in Z21. -/
+/--
+Prove that $B={0,3,4,9,11}$ is a difference set in $Z_{21}$.
+-/
 theorem brualdi_ch10_31 : isDifferenceSet 21 5 {0, 3, 4, 9, 11} where
   size := sorry
   mem_appearance := sorry
