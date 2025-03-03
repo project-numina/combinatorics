cfa75500edfb7704bb4b9eb61459d7d789e9d7e9
ahhwuhu <liujunqi@amss.ac.cn>
2025-03-03 22:06:07 +0800

    some fix

--- CombiBench/imo_2023_p5.lean
+++ CombiBench/imo_2023_p5.lean
@@ -1,13 +1,11 @@
 import Mathlib
 
--- k = \lfloor \log_(n) \rfloor + 1
-
 structure Index (n : ℕ+) where
   row : ℕ
   col : ℕ
   le_row : 1 ≤ row -- counting from 1
   row_le : row ≤ n
-  le_col : 1 ≤ row -- counting from 1
+  le_col : 1 ≤ col -- counting from 1
   col_le : col ≤ row
 
 /-
@@ -25,17 +23,6 @@ def Index.atBottomLeft {n : ℕ+} (p q : Index n) : Prop :=
 -/
 def Index.atBottomRight {n : ℕ+} (p q : Index n) : Prop :=
   q.row + 1 = p.row ∧ q.col + 1 = p.col
-
-@[simp]
-lemma Index.atBottomLeft_irrefl {n : ℕ+} (p : Index n) : p.atBottomLeft p ↔ false := by
-  simp [atBottomLeft]
-
-
-
-@[simp]
-lemma Index.atBottomRight_irrefl {n : ℕ+} (p : Index n) : p.atBottomRight p ↔ false := by
-  simp [atBottomRight]
-
 
 @[simps]
 def triangleGraph (n : ℕ+) : Digraph (Index n) where
@@ -61,13 +48,12 @@ def NinjaPath.countRed {n : ℕ+} {jt : JapaneseTriangle n} (p : NinjaPath jt) :
     then 1
     else 0
 
-def sol : ℕ+ → ℕ := sorry
-
-def sol_cond (n : ℕ+) := ∀ (jt : JapaneseTriangle n), ∃ (p : NinjaPath jt), (sol n) ≤ p.countRed
-
-def sol_max  (n : ℕ+) := ∃ (jt : JapaneseTriangle n), ∃ (p : NinjaPath jt), p.countRed < (sol n).succ
+def imo_2023_p5_solution (n : ℕ+) : ℕ := sorry
 
 /--
 Let $n$ be a positive integer. A Japanese triangle consists of $1 + 2 + \dots + n$ circles arranged in an equilateral triangular shape such that for each $i = 1$, $2$, $\dots$, $n$, the $i^{th}$ row contains exactly $i$ circles, exactly one of which is coloured red. A ninja path in a Japanese triangle is a sequence of $n$ circles obtained by starting in the top row, then repeatedly going from a circle to one of the two circles immediately below it and finishing in the bottom row. In terms of $n$, find the greatest $k$ such that in each Japanese triangle there is a ninja path containing at least $k$ red circles.
 -/
-theorem imo_2023_p5 : ∀ n, sol_cond n ∧ sol_max n := sorry
+theorem imo_2023_p5 (n : ℕ+) :
+  IsGreatest {k | ∀ (jt : JapaneseTriangle n), ∃ (p : NinjaPath jt), k ≤ p.countRed}
+  (imo_2023_p5_solution n) := by
+  sorry
