94dc7b0a185ce2057ad8dfbfd1eab334926b3164
97l <130048468+ahhwuhu@users.noreply.github.com>
2025-03-03 15:35:40 +0800

    Update brualdi_ch4_59.lean

--- CombiBench/brualdi_ch4_59.lean
+++ CombiBench/brualdi_ch4_59.lean
@@ -1,6 +1,8 @@
 import Mathlib
 
-/-- Let n≥2 be an integer. Prove that the total number of inversions of all n ! permutations of 1,2,…,n equals 12n!(n2)=n!n(n−1)4 (Hint: Pair up the permutations so that the number of inversions in each pair is n(n−1)/2.)-/
+/-- 
+Let $n \geq 2$ be an integer. Prove that the total number of inversions of all $n$ ! permutations of $1,2, \ldots, n$ equals $\frac{1}{2} n!\binom{n}{2}=n!\frac{n(n-1)}{4}$ (Hint: Pair up the permutations so that the number of inversions in each pair is $n(n-1) / 2$.)
+-/
 theorem brualdi_ch4_59 (n : ℕ) : ∑ σ : Equiv.Perm (Fin n),
     (Equiv.Perm.swapFactors (α := Fin n) σ).1.length =
     n! * n * (n-1) / 4 := sorry
