8a1ede4b04d6ad610191cd9df05fa23313583a88
97l <130048468+ahhwuhu@users.noreply.github.com>
2025-03-03 16:40:18 +0800

    Update brualdi_ch8_9.lean

--- CombiBench/brualdi_ch8_9.lean
+++ CombiBench/brualdi_ch8_9.lean
@@ -2,6 +2,8 @@ import Mathlib
 
 abbrev diff : (ℕ → ℤ) → (ℕ → ℤ) := fun a ↦ fun i ↦ a (i + 1) - a i
 
-/-- Prove that the following formula holds for the k th-order differences of a sequence h0,h1,…,hn,… :[\Delta^{k} h_{n}=\sum_{j=0}^{k}(-1)^{k-j}\binom{k}{j} h_{n+j}]-/
+/--
+Prove that the following formula holds for the $k$ th-order differences of a sequence $h_{0}, h_{1}, \ldots, h_{n}, \ldots: \Delta^{k} h_{n}=\sum_{j=0}^{k}(-1)^{k-j}\binom{k}{j} h_{n+j}$.
+-/
 theorem brualdi_ch8_9 (h : ℕ → ℤ) (k n : ℕ): (diff^k) h n = ∑ j : Fin (k + 1),
     (-1 : ℤ)^(k-j.1) * Nat.choose k j * h (n + j) := sorry
