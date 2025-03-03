f09b53e9351facdf76c5ba00d00034b61aabcca1
97l <130048468+ahhwuhu@users.noreply.github.com>
2025-03-03 16:52:00 +0800

    Update brualdi_ch6_9.lean

--- CombiBench/brualdi_ch6_9.lean
+++ CombiBench/brualdi_ch6_9.lean
@@ -1,8 +1,11 @@
 import Mathlib
 
+open Finset
+
 abbrev brualdi_ch6_9_solution : ℕ := sorry
 
-open Finset in
-/--Determine the number of integral solutions of the equation x1+x2+x3+x4=20 that satisfy 1≤x1≤6, 0≤x2≤7, 4≤x3≤8, 2≤x4≤6.-/
-theorem brualdi_ch6_9 : {x : Fin 4 → ℕ | x0 ∈ Icc 1 6 ∧ x 1 ∈ Icc 0 7 ∧
+/--
+Determine the number of integral solutions of the equation $x_{1}+x_{2}+x_{3}+x_{4}=20$ that satisfy $1 \leq x_{1} \leq 6,0 \leq x_{2} \leq 7,4 \leq x_{3} \leq 8,2 \leq x_{4} \leq 6$.
+-/
+theorem brualdi_ch6_9 : {x : Fin 4 → ℕ | x 0 ∈ Icc 1 6 ∧ x 1 ∈ Icc 0 7 ∧
   x 2 ∈ Icc 4 8 ∧ x 3 ∈ Icc 2 6}.ncard = brualdi_ch6_9_solution := by sorry
