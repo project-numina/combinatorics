bd3bc2025a0db32b877ba98381e84be0674ccdb7
97l <130048468+ahhwuhu@users.noreply.github.com>
2025-03-03 15:33:57 +0800

    Update brualdi_ch5_26.lean

--- CombiBench/brualdi_ch5_26.lean
+++ CombiBench/brualdi_ch5_26.lean
@@ -1,6 +1,8 @@
 import Mathlib
 
-/--Let n and k be integers with 1≤k≤n. Prove that ∑k=1n(nk)(nk−1)=12(2n+1n+1)−(2nn).-/
+/--
+Let $n$ and $k$ be integers with $1 \leq k \leq n$. Prove that $\sum_{k=1}^{n}\binom{n}{k}\binom{n}{k-1}=\frac{1}{2}\binom{2 n+1}{n+1}-\binom{2 n}{n}$.
+-/
 theorem brualdi_ch5_26 (n k : ℕ) (h1 : 1 ≤ k) (h2 : k ≤ n) :
     ∑ k : Fin n, Nat.choose n (k.1 + 1) * Nat.choose n k.1 =
     (1 / 2 : ℚ) * Nat.choose (2 * n + 1) (n + 1) - Nat.choose (2 * n) n := sorry
