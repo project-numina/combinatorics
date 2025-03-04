e1fd40e33154bddb9f916ceed893a6ea343ba4e5
ahhwuhu <liujunqi@amss.ac.cn>
2025-03-04 16:45:29 +0800

    fix

--- CombiBench/imo_2015_p6.lean
+++ CombiBench/imo_2015_p6.lean
@@ -1,18 +1,9 @@
 import Mathlib
 
-variable (a : ℕ+ → ℤ) (ha1 : ∀ j : ℕ+, 1 ≤ a j ∧ a j ≤ 2015)
-  (ha2 : ∀ k l : ℕ+, k < l → k + a k ≠ l + a l)
-
-/-- answer should b, N in the question.-/
-def answer: Fin 2 → ℕ+ := ![sorry, sorry]
-
 /--
-The sequence $a_1,a_2,\dots$ of integers satisfies the conditions:
-(i) $1\le a_j\le2015$ for all $j\ge1$,
-(ii) $k+a_k\neq \ell+a_\ell$ for all $1\le k<\ell$.
-Prove that there exist two positive integers $b$ and $N$
-for which\[\left\vert\sum_{j=m+1}^n(a_j-b)\right\vert\le1007^2\]
-for all integers $m$ and $n$ such that $n>m\ge N$.
+The sequence $a_1,a_2,\dots$ of integers satisfies the conditions: (i) $1\le a_j\le2015$ for all $j\ge1$, (ii) $k+a_k\neq \ell+a_\ell$ for all $1\le k<\ell$. Prove that there exist two positive integers $b$ and $N$ for which\[\left\vert\sum_{j=m+1}^n(a_j-b)\right\vert\le1007^2\]for all integers $m$ and $n$ such that $n>m\ge N$.
 -/
-theorem imo_2015_p6 (n m : ℕ+) (hn : answer 1 < n) (hm : answer 2 < m) (h : m < n):
-  |(∑ j ∈ Set.Icc (m + 1) n, (a j - answer 0))| ≤ 1007^2 := by sorry
+theorem imo_2015_p6 (a : ℕ+ → ℤ) (ha1 : ∀ j : ℕ+, 1 ≤ a j ∧ a j ≤ 2015)
+  (ha2 : ∀ k l, k < l → k + a k ≠ l + a l) :
+  ∃ b N : ℕ+, ∀ m n, n > m ∧ m ≥ N → |(∑ j ∈ Finset.Icc (m + 1) n, (a j - b))| ≤ 1007^2 := by
+  sorry
