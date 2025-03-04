73363aca0d25312ee40ade85925502deb20a936e
Jonas Bayer <jonas.bayer999@gmail.com>
2025-03-04 11:00:37 +0000

    Refactor: Improve problem presentation

--- CombiBench/bxmo_2017_p2.lean
+++ CombiBench/bxmo_2017_p2.lean
@@ -1,38 +1,79 @@
 import Mathlib
 
-variable (n : ℕ) (h_n : n ≥ 2)
+-- It is simpler to work with the number of islands that don't have a factory. We call that number m.
+variable (m : ℕ)
 
 -- Convention: We rename the islands such that the first factory is on island zero and the second factory is on island one.
+local notation3 (prettyPrint := false) "n" => (m + 2)
+local notation3 (prettyPrint := false) "F1" => (0 : Fin n)
+local notation3 (prettyPrint := false) "F2" => (1 : Fin n)
 
-abbrev GameState := SimpleGraph (Fin n)
+structure GameState where
+  islands: SimpleGraph (Fin n)
+  decidable: DecidableRel islands.Adj
 
-def GameState.initial : GameState n := ⊥
+instance (s : GameState m) : DecidableRel s.islands.Adj := by
+  exact s.decidable
 
-structure Move where
+def GameState.initial : GameState m := {
+  islands := ⊥
+  decidable := SimpleGraph.Bot.adjDecidable (Fin n)
+}
+
+structure Bridge where
   island1 : Fin n
   island2 : Fin n
-  valid: island1 ≠ island2
 
-def GameState.next {n : ℕ} (s : GameState n) (m : Move n) : GameState n :=
-  s ⊔ SimpleGraph.fromRel λ i j => (i = m.island1 ∧ j = m.island2) ∨ (j = m.island1 ∧ i = m.island2)
+def reachableByFactory (s : GameState m) (b : Bridge m) : Prop :=
+  s.islands.Reachable b.island1 F1 ∨ s.islands.Reachable b.island1 F2
+  ∨ s.islands.Reachable b.island2 F1 ∨ s.islands.Reachable b.island2 F2
 
-def GameState.is_losing_state {n : ℕ} (h_n : n ≥ 2) (s : GameState n) : Prop :=
-  Nonempty (s.Path ⟨0, by linarith [h_n]⟩ ⟨1, by linarith [h_n]⟩ )
+def isValidMove (s : GameState m) (b : Bridge m) : Prop :=
+  b.island1 ≠ b.island2 ∧ ¬ s.islands.Adj b.island1 b.island2 ∧ reachableByFactory m s b
 
-abbrev Strategy := GameState n → Move n
+def GameState.next (s : GameState m) (b : Bridge m) : GameState m := {
+  islands := s.islands ⊔ (SimpleGraph.fromEdgeSet {s(b.island1, b.island2)})
+  decidable := by
+    have newEdge: DecidableRel (SimpleGraph.fromEdgeSet {s(b.island1, b.island2)}).Adj := by
+      intro x y; unfold SimpleGraph.fromEdgeSet
+      simp only [Pi.inf_apply, Sym2.toRel_prop, Set.mem_singleton_iff, Sym2.eq, Sym2.rel_iff',
+        Prod.mk.injEq, Prod.swap_prod_mk, ne_eq, inf_Prop_eq]
+      infer_instance
+    exact SimpleGraph.Sup.adjDecidable (Fin n) s.islands (SimpleGraph.fromEdgeSet {s(b.island1, b.island2)})
+}
 
-instance (s: GameState n) : Decidable (GameState.is_losing_state h_n s) := sorry
+def GameState.is_losing_state (s : GameState m) : Prop :=
+  s.islands.Reachable F1 F2
 
-partial def aliceWins (s : GameState n) (s1: Strategy n) (s2: Strategy n): Bool :=
-  let state_after_player1 := s.next (s1 s);
-  if state_after_player1.is_losing_state h_n then
-    False
+abbrev Strategy := GameState m → Bridge m
+
+instance (s: GameState m) : Decidable (GameState.is_losing_state m s) := by
+  simp [GameState.is_losing_state]; infer_instance
+
+instance (s: GameState m) (b : Bridge m) : Decidable (reachableByFactory m s b) := by
+  simp [reachableByFactory]; infer_instance
+
+instance (s: GameState m) (b : Bridge m) : Decidable (isValidMove m s b) := by
+  simp [isValidMove]; infer_instance
+
+structure MoveOutcome where
+  nextState : GameState m
+  hasLost : Bool
+
+def executeStrategy (s : GameState m) (strategy: Strategy m): MoveOutcome m :=
+  let bridge := strategy s
+  if ¬ isValidMove m s bridge
+    then { nextState := s, hasLost := true }
   else
-    let state_after_player_2 := state_after_player1.next (s2 state_after_player1);
-    if state_after_player_2.is_losing_state h_n then
-      True
-    else
-      aliceWins state_after_player_2 s1 s2
+    let nextState := s.next m bridge
+    { nextState := nextState, hasLost := nextState.is_losing_state m }
+
+partial def aliceWins (s : GameState m) (sA: Strategy m) (sB: Strategy m): Bool :=
+  let ⟨stateAfterAlicesMove, aliceHasLost⟩ := executeStrategy m s sA;
+  if aliceHasLost then False else
+  let ⟨stateAfterBobsMove, bobHasLost⟩ := executeStrategy m stateAfterAlicesMove sB;
+  if bobHasLost then True else
+  aliceWins stateAfterBobsMove sA sB
 
 def solution_set : Set ℕ := sorry
 
@@ -47,7 +88,7 @@ As soon as a player builds a bridge that makes it possible to go from one factor
 (Note: It is allowed to construct a bridge passing above another bridge.)
 -/
 theorem bxmo_2017_p2 : (n ∈ solution_set →
-  ∃ strategyA , ∀ strategyB, aliceWins n h_n (GameState.initial n) strategyA strategyB)
+  ∃ strategyA , ∀ strategyB, aliceWins m (GameState.initial m) strategyA strategyB)
   ∧ (n ∉ solution_set →
-  ∃ strategyB, ∀ strategyA, ¬ aliceWins n h_n (GameState.initial n) strategyA strategyB)
+  ∃ strategyB, ∀ strategyA, ¬ aliceWins m (GameState.initial m) strategyA strategyB)
   := by sorry
