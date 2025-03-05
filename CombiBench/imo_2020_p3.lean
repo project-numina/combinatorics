e76167d40b86ce67badd993a102dcfede1659523
ahhwuhu <liujunqi@amss.ac.cn>
2025-03-05 18:24:59 +0800

    fix imo_2020_p3

--- CombiBench/imo_2020_p3.lean
+++ CombiBench/imo_2020_p3.lean
@@ -1,36 +1,18 @@
 import Mathlib
 
-namespace PebbleGame
-
-open Finset
-
-
 structure Pebble (n : ℕ) where
   weight : ℕ
   color : Fin n
-deriving Repr
+deriving DecidableEq
 
-instance {n : ℕ} : DecidableEq (Pebble n) :=
-  fun x y =>
-    if h : x.weight = y.weight ∧ x.color = y.color then
-      isTrue (by cases x; cases y; simp at h; simp [h])
-    else
-      isFalse (by intro H; cases x; cases y; simp at H; exact h H)
-
-def PebbleSet (n : ℕ) (h : 0 < n): Multiset (Pebble n) :=
-  (Multiset.range (4 * n)).map (λ i => ⟨i + 1, ⟨i % n, Nat.mod_lt i h⟩⟩)
-
-
-def ValidSplit (n : ℕ) (S : Finset (Pebble n)) (h : 0 < n) : Prop :=
-  let T := (PebbleSet n h).toFinset \ S
-  (S.sum Pebble.weight = T.sum Pebble.weight) ∧
-  (∀ c : Fin n, (S.filter (λ p => p.color = c)).card = 2)
-
-/-- 708
+/--
 There are $4n$ pebbles of weights $1, 2, 3, \ldots, 4n$. Each pebble is colored in one of $n$ colors and there are four pebbles of each color. Show that we can arrange the pebbles into two piles so that the following two conditions are both satisfied: 1) The total weights of both piles are the same. 2) Each pile contains two pebbles of each color.
 -/
-theorem imo_2020_p3  (n : ℕ) (h : 0 < n) :
-  ∃ S : Finset (Pebble n), ValidSplit n S h := by
+theorem imo_2020_p3 (n : ℕ) (PebbleSet : Finset (Pebble n)) (hP : PebbleSet.card = 4 * n)
+  (h_weight : ∀ p ∈ PebbleSet, p.weight ∈ Finset.Icc 1 (4 * n))
+  (h_ne_weight : ∀ p ∈ PebbleSet, ∀ q ∈ PebbleSet, p ≠ q → p.weight ≠ q.weight)
+  (h_color : ∀ i, (PebbleSet.filter (fun p => p.color = i)).card = 4) :
+  ∃ (P1 P2 : Finset (Pebble n)), P1 ∪ P2 = PebbleSet ∧ P1 ∩ P2 = ∅ ∧
+  ∑ p ∈ P1, p.weight = ∑ p ∈ P2, p.weight ∧ (∀ i, (P1.filter (fun p => p.color = i)).card = 2) ∧
+  (∀ i, (P2.filter (fun p => p.color = i)).card = 2) := by
   sorry
-
-end PebbleGame
