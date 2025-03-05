32e52cfa5debd1f3918b65b2f5dcecad62d30e46
xbb <xbbng.0@gmail.com>
2025-03-05 23:11:56 +0800

    fix jbmo_2012_p3

--- CombiBench/jbmo_2012_p3.lean
+++ CombiBench/jbmo_2012_p3.lean
@@ -3,6 +3,6 @@ import Mathlib
 /--
 On a board there are $n$ nails each two connected by a string. Each string is colored in one of $n$ given distinct colors. For each three distinct colors, there exist three nails connected with strings in these three colors. Can $n$ be\na) 6 ?\nb) 7 ?
 -/
-theorem jbmo_2012_p3 (n :ℕ )   (e₁ e₂ e₃:  Sym2 (Finset (Fin n))) (he₁ : e₁∈ completeGraph_S.edgeSet) (he₁ : e₂ ∈ completeGraph_S.edgeSet)
-(he₁ : e₃ ∈ completeGraph_S.edgeSet) (f : Sym2 (Finset (Fin n)) → Fin n ) :
-  if ∃ e₁ e₂ e₃, f e₁ ≠  f e₂ ∧ f e₃ ≠  f e₂ ∧  f e₁ ≠  f e₃ ∧ e₁ ≠ e₂ ∧ e₁ ≠ e₃ ∧ e₂ ≠ e₃ then True else False:= by sorry
+theorem jbmo_2012_p3 (n : ℕ) (G : SimpleGraph V) (e₁ e₂ e₃: Sym2 V) (he₁ : e₁∈ G.edgeSet) (he₁ : e₂ ∈ G.edgeSet)
+(he₁ : e₃ ∈ G.edgeSet) (f : Sym2 (Finset (Fin n)) → Fin n ) :
+ if ∃ e₁ e₂ e₃, f e₁ ≠ f e₂ ∧ f e₃ ≠ f e₂ ∧ f e₁ ≠ f e₃ ∧ e₁ ≠ e₂ ∧ e₁ ≠ e₃ ∧ e₂ ≠ e₃ then True else False:= by sorry
