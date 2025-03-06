a8fea80c6190496ff78a856510baceefd2321bc7
ahhwuhu <liujunqi@amss.ac.cn>
2025-03-06 16:25:57 +0800

    draft jbmo

--- CombiBench/jbmo_2012_p3.lean
+++ CombiBench/jbmo_2012_p3.lean
@@ -1,5 +1,8 @@
 import Mathlib
 
+def condition (n : ℕ) (G : SimpleGraph V) (color : Sym2 V → Fin n) : Prop :=
+    ∀ i j k : Fin n, ∃ v1 v2 v3 : V, s(v1, v2) ∈ G.edgeSet ∧ s(v2, v3) ∈ G.edgeSet ∧
+    s(v3, v1) ∈ G.edgeSet ∧ color s(v1, v2) = i ∧ color s(v2, v3) = j ∧ color s(v3, v1) = k
 
 abbrev jbmo_2012_p3_solution_a : Prop := sorry
 abbrev jbmo_2012_p3_solution_b : Prop := sorry
@@ -7,7 +10,11 @@ abbrev jbmo_2012_p3_solution_b : Prop := sorry
 /--
 On a board there are $n$ nails each two connected by a string. Each string is colored in one of $n$ given distinct colors. For each three distinct colors, there exist three nails connected with strings in these three colors. Can $n$ be\na) 6 ?\nb) 7 ?
 -/
-theorem jbmo_2012_p3 (n : ℕ) (G : SimpleGraph V) (hG : G = completeGraph V) (color : Sym2 V → Fin n)
-    (h : ∀ i j k : Fin n, ∃ v1 v2 v3 : V, s(v1, v2) ∈ G.edgeSet ∧ s(v2, v3) ∈ G.edgeSet ∧
-    s(v3, v1) ∈ G.edgeSet ∧ color s(v1, v2) = i ∧ color s(v2, v3) = j ∧ color s(v3, v1) = k) :
-    n = 6 := by sorry
+theorem jbmo_2012_p3 {nails : Type} [Fintype nails] (n : ℕ) (hn : Fintype.card nails = n)
+  (G : SimpleGraph nails) (hG : G = completeGraph nails) (color : Sym2 nails → Fin n) :
+  1 =1  := by
+
+
+
+
+  sorry
