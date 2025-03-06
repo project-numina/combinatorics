7f7d1c81c042db6a8546cf0184d69b9b6ad4c125
ahhwuhu <liujunqi@amss.ac.cn>
2025-03-06 17:01:02 +0800

    def invNum

--- CombiBench/brualdi_ch4_59.lean
+++ CombiBench/brualdi_ch4_59.lean
@@ -1,8 +1,10 @@
 import Mathlib
 
-/-- 
+def invNum {n : ℕ} (σ : Equiv.Perm (Fin n)) : ℕ :=
+  ∑ x ∈ Equiv.Perm.finPairsLT n, if σ x.fst ≤ σ x.snd then 0 else 1
+
+/--
 Let $n \geq 2$ be an integer. Prove that the total number of inversions of all $n$ ! permutations of $1,2, \ldots, n$ equals $\frac{1}{2} n!\binom{n}{2}=n!\frac{n(n-1)}{4}$ (Hint: Pair up the permutations so that the number of inversions in each pair is $n(n-1) / 2$.)
 -/
-theorem brualdi_ch4_59 (n : ℕ) : ∑ σ : Equiv.Perm (Fin n),
-    (Equiv.Perm.swapFactors (α := Fin n) σ).1.length =
-    n! * n * (n-1) / 4 := sorry
+theorem brualdi_ch4_59 (n : ℕ) : ∑ σ : Equiv.Perm (Fin n), invNum σ =
+    n! * n * (n-1) / 4 := by sorry
