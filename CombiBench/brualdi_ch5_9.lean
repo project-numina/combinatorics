d735ee9d3ea8a57a840d0b8eb71700e7464cad11
ahhwuhu <liujunqi@amss.ac.cn>
2025-03-09 14:44:20 +0800

    fix

--- CombiBench/brualdi_ch5_9.lean
+++ CombiBench/brualdi_ch5_9.lean
@@ -1,9 +1,9 @@
 import Mathlib
 
-abbrev brualdi_ch5_9_solution : ℤ := sorry
+abbrev brualdi_ch5_9_solution (n : ℕ) : ℤ := sorry
 
 /--
 Evaluate the sum $\sum_{k=0}^{n}(-1)^{k}\binom{n}{k} 10^{k}$.
 -/
-theorem brualdi_ch5_9 : ∑ k ∈ Finset.range (n + 1), (-1 : ℤ) ^ k * (n.choose k) * 10 ^ k =
-    brualdi_ch5_9_solution := by sorry
+theorem brualdi_ch5_9 (n : ℕ) : ∑ k ∈ Finset.range (n + 1), (-1 : ℤ) ^ k * (n.choose k) * 10 ^ k =
+    brualdi_ch5_9_solution n := by sorry
