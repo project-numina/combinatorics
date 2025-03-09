5195240b9f726d106c87524a289c2a6baa6653a6
ahhwuhu <liujunqi@amss.ac.cn>
2025-03-09 12:37:22 +0800

    align to main

--- CombiBench/imo_2020_p3.lean
+++ CombiBench/imo_2020_p3.lean
@@ -1,18 +1,7 @@
 import Mathlib
 
-structure Pebble (n : ℕ) where
-  weight : ℕ
-  color : Fin n
-deriving DecidableEq
-
-/--
+/-- 708
 There are $4n$ pebbles of weights $1, 2, 3, \ldots, 4n$. Each pebble is colored in one of $n$ colors and there are four pebbles of each color. Show that we can arrange the pebbles into two piles so that the following two conditions are both satisfied: 1) The total weights of both piles are the same. 2) Each pile contains two pebbles of each color.
 -/
-theorem imo_2020_p3 (n : ℕ) (PebbleSet : Finset (Pebble n)) (hP : PebbleSet.card = 4 * n)
-  (h_weight : ∀ p ∈ PebbleSet, p.weight ∈ Finset.Icc 1 (4 * n))
-  (h_ne_weight : ∀ p ∈ PebbleSet, ∀ q ∈ PebbleSet, p ≠ q → p.weight ≠ q.weight)
-  (h_color : ∀ i, (PebbleSet.filter (fun p => p.color = i)).card = 4) :
-  ∃ (P1 P2 : Finset (Pebble n)), P1 ∪ P2 = PebbleSet ∧ P1 ∩ P2 = ∅ ∧
-  ∑ p ∈ P1, p.weight = ∑ p ∈ P2, p.weight ∧ (∀ i, (P1.filter (fun p => p.color = i)).card = 2) ∧
-  (∀ i, (P2.filter (fun p => p.color = i)).card = 2) := by
-  sorry
+theorem imo_2020_p3 : 1 = 1
+    := by sorry
--- CombiBench/usamo_2000_p4.lean
+++ CombiBench/usamo_2000_p4.lean
@@ -1,13 +1,7 @@
 import Mathlib
 
-def colored_card : Finset ℕ :=
-  (Finset.image (fun s => s.card)
-  (@Finset.univ (Finset (Fin 1000 × Fin 1000)) _ |>.filter
-  (fun s => ∃ a ∈ s, ∃ b ∈ s, ∃ c ∈ s, a.1 = b.1 ∧ a.2 = b.2)))
-
-abbrev usamo_2000_p4_solution : ℕ+ := sorry
-
 /--
 Find the smallest positive integer $n$ such that if $n$ squares of a $1000 \\times 1000$ chessboard are colored, then there will exist three colored squares whose centers form a right triangle with sides parallel to the edges of the board.
 -/
-theorem usamo_2000_p4 : IsLeast colored_card usamo_2000_p4_solution.1 := by sorry
+theorem usamo_2000_p4 : 1 = 1
+    := by sorry
