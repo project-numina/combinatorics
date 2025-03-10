878e9360707a15545509eb621ca1cb679316de94
ahhwuhu <liujunqi@amss.ac.cn>
2025-03-10 20:40:09 +0800

    fix

--- CombiBench/brualdi_ch7_27.lean
+++ CombiBench/brualdi_ch7_27.lean
@@ -1,12 +1,14 @@
 import Mathlib
 
-abbrev S (n : ℕ): Finset ℕ :=
+abbrev S (n : ℕ) : Finset ℕ :=
   {m < 10^n | (Nat.digits 10 m).length = n ∧
   (∀ i : Fin (Nat.digits 10 m).length, Odd ((Nat.digits 10 m).get i)) ∧
   Even ((Nat.digits 10 m).count 1) ∧ Even ((Nat.digits 10 m).count 3) ∧
   ((Nat.digits 10 m).count 1) ≠ 0 ∧ ((Nat.digits 10 m).count 3) ≠ 0}
 
-abbrev brualdi_ch7_27_solution (n : ℕ): ℕ := sorry
+abbrev brualdi_ch7_27_solution : ℕ → ℕ := sorry
 
-/-- Determine the number of n-digit numbers with all digits odd, such that 1 and 3 each occur a nonzero, even number of times.-/
-theorem brualdi_ch7_27 (n : ℕ): (S n).card = brualdi_ch7_27_solution := sorry
+/--
+Determine the number of n-digit numbers with all digits odd, such that 1 and 3 each occur a nonzero, even number of times.
+-/
+theorem brualdi_ch7_27 (n : ℕ) : (S n).card = brualdi_ch7_27_solution n := by sorry
