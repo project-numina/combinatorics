07328aa5703cd112ce634bc6968bf0911553ad4a
ahhwuhu <liujunqi@amss.ac.cn>
2025-03-13 00:58:05 +0800

    delete variable

--- CombiBench/brualdi_ch1_16.lean
+++ CombiBench/brualdi_ch1_16.lean
@@ -1,7 +1,5 @@
 import Mathlib
 
-variable (n s : ℕ)
-
 @[ext]
 structure isMagicSquare (n s : ℕ) (M : Matrix (Fin n) (Fin n) ℕ) : Prop where
   mem : ∀ i j, M i j ∈ Finset.Icc 1 (n * n)
@@ -9,7 +7,7 @@ structure isMagicSquare (n s : ℕ) (M : Matrix (Fin n) (Fin n) ℕ) : Prop wher
   column_sum : ∀ i, ∑ j, M i j = s
   row_sum : ∀ j, ∑ i, M i j = s
 
-abbrev replace : Matrix (Fin n) (Fin n) ℕ → Matrix (Fin n) (Fin n) ℕ :=
+abbrev replace (n : ℕ) : Matrix (Fin n) (Fin n) ℕ → Matrix (Fin n) (Fin n) ℕ :=
   fun A i j ↦ n^2 + 1 - A i j
 
 /-- Show that the result of replacing every integer a in a magic square of order n
