5905166e6e55d6e01f6256fa09636185c09500a8
ahhwuhu <liujunqi@amss.ac.cn>
2025-03-13 16:41:11 +0800

    change problem

--- CombiBench/easy_3.lean
+++ CombiBench/easy_3.lean
@@ -5,4 +5,6 @@ abbrev easy_3_solution : ℕ := sorry
 /--
 How many four-digit numbers can be formed from the numbers 3 5 8 9 if they are not allowed to be repeated?
 -/
-theorem easy_3 : 1 = 1 := by sorry
+theorem easy_3 (sol : Finset ℕ)
+  (h_sol : ∀ s ∈ sol, 1000 ≤ s ∧ s ≤ 9999 ∧ (Nat.digits 10 s).toFinset = {3, 5, 8, 9}) :
+  sol.card = easy_3_solution := by sorry
--- CombiBench/easy_4.lean
+++ CombiBench/easy_4.lean
@@ -5,4 +5,4 @@ abbrev easy_4_solution : ℕ := sorry
 /--
 How many people must be in a group for at least two of them to be born in the same month?
 -/
-theorem easy_4 : 1 = 1 := by sorry
+theorem easy_4 : IsLeast {n | ∀ f : Fin n → Fin 12, ∃ a b, f a = f b} easy_4_solution := by sorry
