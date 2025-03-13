5d836837e3fd0a002d7c3f786397a0cac2a77023
ahhwuhu <liujunqi@amss.ac.cn>
2025-03-13 12:05:00 +0800

    format

--- CombiBench/imo_2010_p5.lean
+++ CombiBench/imo_2010_p5.lean
@@ -1,50 +1,48 @@
 import Mathlib
 
--- Ture
-
 structure Bottles where
   (B1 B2 B3 B4 B5 B6 : ℕ)
 
 def op11 : Bottles → Bottles
-| ⟨b1, b2, b3, b4, b5, b6⟩ => ⟨b1 - 1, b2 + 2, b3, b4, b5, b6⟩
+  | ⟨b1, b2, b3, b4, b5, b6⟩ => ⟨b1 - 1, b2 + 2, b3, b4, b5, b6⟩
 
 def op12 : Bottles → Bottles
-| ⟨b1, b2, b3, b4, b5, b6⟩ => ⟨b1, b2 - 1, b3 + 2, b4, b5, b6⟩
+  | ⟨b1, b2, b3, b4, b5, b6⟩ => ⟨b1, b2 - 1, b3 + 2, b4, b5, b6⟩
 
 def op13 : Bottles → Bottles
-| ⟨b1, b2, b3, b4, b5, b6⟩ => ⟨b1, b2, b3 - 1, b4 + 2, b5, b6⟩
+  | ⟨b1, b2, b3, b4, b5, b6⟩ => ⟨b1, b2, b3 - 1, b4 + 2, b5, b6⟩
 
 def op14 : Bottles → Bottles
-| ⟨b1, b2, b3, b4, b5, b6⟩ => ⟨b1, b2, b3, b4 - 1, b5 + 2, b6⟩
+  | ⟨b1, b2, b3, b4, b5, b6⟩ => ⟨b1, b2, b3, b4 - 1, b5 + 2, b6⟩
 
 def op15 : Bottles → Bottles
-| ⟨b1, b2, b3, b4, b5, b6⟩ => ⟨b1, b2, b3, b4, b5 - 1, b6 + 2⟩
+  | ⟨b1, b2, b3, b4, b5, b6⟩ => ⟨b1, b2, b3, b4, b5 - 1, b6 + 2⟩
 
 def op21 : Bottles → Bottles
-| ⟨b1, b2, b3, b4, b5, b6⟩ => ⟨b1 - 1, b3, b2, b4, b5, b6⟩
+  | ⟨b1, b2, b3, b4, b5, b6⟩ => ⟨b1 - 1, b3, b2, b4, b5, b6⟩
 
 def op22 : Bottles → Bottles
-| ⟨b1, b2, b3, b4, b5, b6⟩ => ⟨b1, b2 - 4, b4, b3, b5, b6⟩
+  | ⟨b1, b2, b3, b4, b5, b6⟩ => ⟨b1, b2 - 4, b4, b3, b5, b6⟩
 
 def op23 : Bottles → Bottles
-| ⟨b1, b2, b3, b4, b5, b6⟩ => ⟨b1, b2, b3 - 1, b5, b4, b6⟩
+  | ⟨b1, b2, b3, b4, b5, b6⟩ => ⟨b1, b2, b3 - 1, b5, b4, b6⟩
 
 def op24 : Bottles → Bottles
-| ⟨b1, b2, b3, b4, b5, b6⟩ => ⟨b1, b2, b3, b4 - 1, b6, b5⟩
+  | ⟨b1, b2, b3, b4, b5, b6⟩ => ⟨b1, b2, b3, b4 - 1, b6, b5⟩
 
 inductive OP
-| op11 | op12 | op13 | op14 | op15 | op21 | op22 | op23 | op24
+  | op11 | op12 | op13 | op14 | op15 | op21 | op22 | op23 | op24
 
 def apply_op : OP → (Bottles → Bottles)
-| OP.op11 => op11
-| OP.op12 => op12
-| OP.op13 => op13
-| OP.op14 => op14
-| OP.op15 => op15
-| OP.op21 => op21
-| OP.op22 => op22
-| OP.op23 => op23
-| OP.op24 => op24
+  | OP.op11 => op11
+  | OP.op12 => op12
+  | OP.op13 => op13
+  | OP.op14 => op14
+  | OP.op15 => op15
+  | OP.op21 => op21
+  | OP.op22 => op22
+  | OP.op23 => op23
+  | OP.op24 => op24
 
 def init : Bottles := ⟨1, 1, 1, 1, 1, 1⟩
 
