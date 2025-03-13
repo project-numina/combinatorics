682fe3b982fdc1f3ee1a76e82ec6c04253c20ff9
zjj <zjj.zjj@zjj.zjj>
2025-03-13 01:19:43 +0000

    maybe better?

--- CombiBench/brualdi_ch4_31.lean
+++ CombiBench/brualdi_ch4_31.lean
@@ -1,9 +1,8 @@
 import Mathlib
 
-def brualdi_ch4_31_sol : Finset (Fin 3 ↪ Set.Icc 1 5) := sorry
+def brualdi_ch4_31_sol : Set (Fin 3 ↪ Set.Icc 1 5) := sorry
 
 /--
 Generate the $3$-permutations of ${1,2,3,4,5}$.
 -/
-theorem brualdi_ch4_31 :
-  Finset.univ (α := Fin 3 ↪ Set.Icc 1 5) = brualdi_ch4_31_sol := by sorry
+theorem brualdi_ch4_31 : ∀ (x : Fin 3 ↪ Set.Icc 1 5), x ∈ brualdi_ch4_31_sol := by sorry
