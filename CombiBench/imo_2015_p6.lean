4d127f5db180510ae74e454228f343fdcfc0e044
ahhwuhu <liujunqi@amss.ac.cn>
2025-03-13 11:59:58 +0800

    fix format

--- CombiBench/imo_2015_p6.lean
+++ CombiBench/imo_2015_p6.lean
@@ -5,5 +5,4 @@ The sequence $a_1,a_2,\dots$ of integers satisfies the conditions: (i) $1\le a_j
 -/
 theorem imo_2015_p6 (a : ℕ+ → ℤ) (ha1 : ∀ j : ℕ+, 1 ≤ a j ∧ a j ≤ 2015)
   (ha2 : ∀ k l, k < l → k + a k ≠ l + a l) :
-  ∃ b N : ℕ+, ∀ m n, n > m ∧ m ≥ N → |(∑ j ∈ Finset.Icc (m + 1) n, (a j - b))| ≤ 1007^2 := by
-  sorry
+  ∃ b N : ℕ+, ∀ m n, n > m ∧ m ≥ N → |(∑ j ∈ Finset.Icc (m + 1) n, (a j - b))| ≤ 1007^2 := by sorry
