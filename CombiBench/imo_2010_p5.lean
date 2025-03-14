976adf0a05d47fcae6b2e62b0f679b16977fddf3
ahhwuhu <liujunqi@amss.ac.cn>
2025-03-14 19:09:57 +0800

    fix format

--- CombiBench/imo_2010_p5.lean
+++ CombiBench/imo_2010_p5.lean
@@ -4,45 +4,45 @@ structure Boxes where
   (B1 B2 B3 B4 B5 B6 : ℕ)
 
 def op11 : Boxes → Boxes
-| ⟨b1, b2, b3, b4, b5, b6⟩ => ⟨b1 - 1, b2 + 2, b3, b4, b5, b6⟩
+  | ⟨b1, b2, b3, b4, b5, b6⟩ => ⟨b1 - 1, b2 + 2, b3, b4, b5, b6⟩
 
 def op12 : Boxes → Boxes
-| ⟨b1, b2, b3, b4, b5, b6⟩ => ⟨b1, b2 - 1, b3 + 2, b4, b5, b6⟩
+  | ⟨b1, b2, b3, b4, b5, b6⟩ => ⟨b1, b2 - 1, b3 + 2, b4, b5, b6⟩
 
 def op13 : Boxes → Boxes
-| ⟨b1, b2, b3, b4, b5, b6⟩ => ⟨b1, b2, b3 - 1, b4 + 2, b5, b6⟩
+  | ⟨b1, b2, b3, b4, b5, b6⟩ => ⟨b1, b2, b3 - 1, b4 + 2, b5, b6⟩
 
 def op14 : Boxes → Boxes
-| ⟨b1, b2, b3, b4, b5, b6⟩ => ⟨b1, b2, b3, b4 - 1, b5 + 2, b6⟩
+  | ⟨b1, b2, b3, b4, b5, b6⟩ => ⟨b1, b2, b3, b4 - 1, b5 + 2, b6⟩
 
 def op15 : Boxes → Boxes
-| ⟨b1, b2, b3, b4, b5, b6⟩ => ⟨b1, b2, b3, b4, b5 - 1, b6 + 2⟩
+  | ⟨b1, b2, b3, b4, b5, b6⟩ => ⟨b1, b2, b3, b4, b5 - 1, b6 + 2⟩
 
 def op21 : Boxes → Boxes
-| ⟨b1, b2, b3, b4, b5, b6⟩ => ⟨b1 - 1, b3, b2, b4, b5, b6⟩
+  | ⟨b1, b2, b3, b4, b5, b6⟩ => ⟨b1 - 1, b3, b2, b4, b5, b6⟩
 
 def op22 : Boxes → Boxes
-| ⟨b1, b2, b3, b4, b5, b6⟩ => ⟨b1, b2 - 4, b4, b3, b5, b6⟩
+  | ⟨b1, b2, b3, b4, b5, b6⟩ => ⟨b1, b2 - 4, b4, b3, b5, b6⟩
 
 def op23 : Boxes → Boxes
-| ⟨b1, b2, b3, b4, b5, b6⟩ => ⟨b1, b2, b3 - 1, b5, b4, b6⟩
+  | ⟨b1, b2, b3, b4, b5, b6⟩ => ⟨b1, b2, b3 - 1, b5, b4, b6⟩
 
 def op24 : Boxes → Boxes
-| ⟨b1, b2, b3, b4, b5, b6⟩ => ⟨b1, b2, b3, b4 - 1, b6, b5⟩
+  | ⟨b1, b2, b3, b4, b5, b6⟩ => ⟨b1, b2, b3, b4 - 1, b6, b5⟩
 
 inductive OP
   | op11 | op12 | op13 | op14 | op15 | op21 | op22 | op23 | op24
 
 def apply_op : OP → (Boxes → Boxes)
-| OP.op11 => op11
-| OP.op12 => op12
-| OP.op13 => op13
-| OP.op14 => op14
-| OP.op15 => op15
-| OP.op21 => op21
-| OP.op22 => op22
-| OP.op23 => op23
-| OP.op24 => op24
+  | OP.op11 => op11
+  | OP.op12 => op12
+  | OP.op13 => op13
+  | OP.op14 => op14
+  | OP.op15 => op15
+  | OP.op21 => op21
+  | OP.op22 => op22
+  | OP.op23 => op23
+  | OP.op24 => op24
 
 def init : Boxes := ⟨1, 1, 1, 1, 1, 1⟩
 
@@ -53,5 +53,4 @@ Each of the six boxes $B_1$, $B_2$, $B_3$, $B_4$, $B_5$, $B_6$ initially contain
 -/
 theorem imo_2010_p5 :
   imo_2010_p5_solution = (∃ seq : List OP,
-    (seq.map apply_op).foldl (· ∘ ·) id init = ⟨0, 0, 0, 0, 0, 2010 ^ (2010 ^ 2010)⟩) :=
-  by sorry
+  (seq.map apply_op).foldl (· ∘ ·) id init = ⟨0, 0, 0, 0, 0, 2010 ^ (2010 ^ 2010)⟩) := by sorry
