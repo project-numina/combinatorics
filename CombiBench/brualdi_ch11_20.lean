b31729d8b26ccaa7eb6317b8e348bbd9b580e1ba
ahhwuhu <liujunqi@amss.ac.cn>
2025-03-15 16:10:50 +0800

    fix

--- CombiBench/brualdi_ch11_20.lean
+++ CombiBench/brualdi_ch11_20.lean
@@ -1,8 +1,8 @@
-import Mathlib.Combinatorics.SimpleGraph.Path
-import Mathlib.Data.Set.Card
+import Mathlib
 
-variable (n : ℕ) (V : Type*) [Fintype V] (hV : Fintype.card V = n)
-  (G : SimpleGraph V) (h : (n-1)*(n -2)/2 + 1 ≤ (SimpleGraph.edgeSet G).ncard)
-
-/-- Prove that a graph of order n with at least `(n-1)(n-2)/2 + 1` edges must be connected.-/
-theorem brualdi_ch11_20 : G.Connected := sorry
+/--
+Prove that a graph of order n with at least `(n-1)(n-2)/2 + 1` edges must be connected.
+-/
+theorem brualdi_ch11_20 {V : Type*} [Fintype V] (n : ℕ) (hn : n ≥ 1) (hV : Fintype.card V = n)
+  (G : SimpleGraph V) (h : (n - 1) * (n - 2) / 2 + 1 ≤ (SimpleGraph.edgeSet G).ncard) :
+  G.Connected := by sorry
