cd65c6cc6daa3fe8670e50f7cb7ff377c9cbe90b
ahhwuhu <liujunqi@amss.ac.cn>
2025-03-15 20:41:57 +0800

    fix

--- CombiBench/imo_2015_p1.lean
+++ CombiBench/imo_2015_p1.lean
@@ -12,7 +12,6 @@ abbrev imo_2015_p1_solution : Set ℤ := sorry
 /--
 We say that a finite set $\mathcal{S}$ in the plane is balanced if, for any two different points $A$, $B$ in $\mathcal{S}$, there is a point $C$ in $\mathcal{S}$ such that $AC=BC$. We say that $\mathcal{S}$ is centre-free if for any three points $A$, $B$, $C$ in $\mathcal{S}$, there is no point $P$ in $\mathcal{S}$ such that $PA=PB=PC$. (1) Show that for all integers $n\geq 3$, there exists a balanced set consisting of $n$ points. (1) Determine all integers $n\geq 3$ for which there exists a balanced centre-free set consisting of $n$ points.
 -/
-theorem imo_2015_p1 :
-  (∀ n ≥ 3, ∃ (S : Finset (ℝ × ℝ)), balanced S ∧ S.card = n) ∧
+theorem imo_2015_p1 : (∀ n ≥ 3, ∃ (S : Finset (ℝ × ℝ)), balanced S ∧ S.card = n) ∧
   {n : ℤ | n ≥ 3 ∧ (∃ (S : Finset (ℝ × ℝ)), balanced S ∧ centre_free S ∧ S.card = n)} =
   imo_2015_p1_solution := by sorry
