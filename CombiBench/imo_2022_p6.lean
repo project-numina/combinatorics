3e7ef2e924dc9b874c3a2f8bb6f291e675422c93
ahhwuhu <liujunqi@amss.ac.cn>
2025-03-17 16:39:02 +0800

    delete answer

--- CombiBench/imo_2022_p6.lean
+++ CombiBench/imo_2022_p6.lean
@@ -23,7 +23,7 @@ instance {n : ℕ} (sq : NordicSquare n) : Finite (UphillPath sq) := by
 
 noncomputable instance {n : ℕ} (sq : NordicSquare n) : Fintype (UphillPath sq) := Fintype.ofFinite (UphillPath sq)
 
-abbrev imo_2022_p6_solution : ℕ → ℕ := fun n => 2 * n ^ 2 - 2 * n + 1
+abbrev imo_2022_p6_solution : ℕ → ℕ := sorry
 
 /--
 Let $n$ be a positive integer. A Nordic square is an $n \times n$ board containing all the integers from $1$ to $n^2$ so that each cell contains exactly one number. Two different cells are considered adjacent if they share an edge. Every cell that is adjacent only to cells containing larger numbers is called a valley. An uphill path is a sequence of one or more cells such that: (i) the first cell in the sequence is a valley, (ii) each subsequent cell in the sequence is adjacent to the previous cell, and (iii) the numbers written in the cells in the sequence are in increasing order. Find, as a function of $n$, the smallest possible total number of uphill paths in a Nordic square.
