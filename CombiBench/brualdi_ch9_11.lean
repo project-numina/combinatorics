cb745bb0d2718bed7073052d8aeeff74f45c4498
ahhwuhu <liujunqi@amss.ac.cn>
2025-03-18 17:50:47 +0800

    fix

--- CombiBench/brualdi_ch9_11.lean
+++ CombiBench/brualdi_ch9_11.lean
@@ -1,31 +1,30 @@
-import Mathlib.Algebra.BigOperators.Group.Finset.Defs
-import Mathlib.Data.Fintype.Pi
-import Mathlib.Data.Nat.Factorial.Basic
-import Mathlib.Data.Rat.Defs
+import Mathlib
 
-abbrev A : Fin n → Finset (Fin n) := fun i ↦ ⊤\{i}
-
-@[ext]
-structure SDR (n : ℕ) (A : Fin n → Finset (Fin n)) where
-  toFun : Fin n → Fin n
+structure SDR {n : ℕ} (A : Fin n → Finset α) where
+  toFun : Fin n → α
   mem_Ai : ∀ i, toFun i ∈ A i
   pairwise : ∀ i j, i ≠ j → toFun i ≠ toFun j
 
-instance (n : ℕ) (A : Fin n → Finset (Fin n)): CoeFun (SDR n A) (fun _ => Fin n → Fin n) where
+instance {n : ℕ} (A : Fin n → Finset α) : CoeFun (SDR A) (fun _ => Fin n → α) where
   coe s := s.toFun
 
-noncomputable instance (n : ℕ) (A : Fin n → Finset (Fin n)) : Fintype (SDR n A) := by
+noncomputable instance {n : ℕ} (A : Fin n → Finset α) : Fintype (SDR A) := by
   classical
-  if h : Nonempty (SDR n A) then
-    exact Fintype.ofSurjective (α := (Fin n → Fin n))
-      (fun f ↦ if h1 : (∃(g : SDR n A), f = g) then ⟨f, fun i ↦ by
-          rw [h1.choose_spec]; exact h1.choose.2 i, fun i j hij ↦ by
-          rw [h1.choose_spec]; exact h1.choose.3 i j hij⟩
-        else Classical.choice (α := (SDR n A)) h) <| fun g ↦ ⟨g, by simp⟩
+  let Y := Finset.biUnion (@Finset.univ (Fin n) _) A
+  if h : Nonempty (SDR A) then
+    exact Fintype.ofSurjective (α := (Fin n → Y))
+      (fun f ↦ if h1 : (∃(g : SDR A), ∀ i, f i = g i) then ⟨fun i => f i,
+          fun i ↦ by have ⟨g, hg⟩ := h1; simp [hg, g.mem_Ai],
+          fun i j hij ↦ by have ⟨g, hg⟩ := h1; simp [hg, g.pairwise i j hij]⟩
+        else Classical.choice (α := (SDR A)) h) <| fun g ↦
+          ⟨fun i => ⟨g i, by simp [Y]; use i; simp [g.mem_Ai]⟩, by
+            simp; suffices ∃ (g' : SDR A), ∀ (i : Fin n), g.toFun i = g'.toFun i by simp [this]
+            use g; simp⟩
   else exact fintypeOfNotInfinite (fun h1 ↦ by aesop)
 
-/-- 
-Let $n&gt;1$, and let $\mathcal{A}=\left(A_{1}, A_{2}, \ldots, A_{n}\right)$ be the family of subsets of ${1,2, \ldots, n}$, where $A_{i}={1,2, \ldots, n}-{i}, \quad(i=1,2, \ldots, n)$. Prove that $\mathcal{A}$ has an SDR and that the number of SDRs is the $n$th derangement number $D_{n}$.
+/--
+Let $n > 1$, and let $\mathcal{A}=\left(A_{1}, A_{2}, \ldots, A_{n}\right)$ be the family of subsets of ${1,2, \ldots, n}$, where $A_{i}={1,2, \ldots, n}-{i}, \quad(i=1,2, \ldots, n)$. Prove that $\mathcal{A}$ has an SDR and that the number of SDRs is the $n$th derangement number $D_{n}$.
 -/
-theorem brualdi_ch9_11 (n : ℕ) (h_n: n > 1) : Nonempty (SDR n A) ∧
-  Fintype.card (SDR n A) = n! * (∑ i : Fin n, ((-1)^i.1 : ℤ) * (1/(Nat.factorial i.1) : ℚ)):= sorry
+theorem brualdi_ch9_11 (n : ℕ) (hn : n > 1) (A : Fin n → Finset ℕ)
+  (hA : ∀ i, A i = Finset.Icc 1 n \ {i.1 + 1}) :
+  Nonempty (SDR A) ∧ Fintype.card (SDR A) = numDerangements n := by sorry
