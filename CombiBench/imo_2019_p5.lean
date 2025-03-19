cf56cf88fddc1a37ecefb523f0e1d071c1306d86
ahhwuhu <liujunqi@amss.ac.cn>
2025-03-19 22:41:28 +0800

    fix

--- CombiBench/imo_2019_p5.lean
+++ CombiBench/imo_2019_p5.lean
@@ -1,7 +1,5 @@
 import Mathlib
 
--- $\frac{n(n+1)}{4}$
-
 variable (n : ℕ)
 
 abbrev Coin := Fin 2
@@ -10,39 +8,31 @@ abbrev Coin.H : Coin := 0
 
 abbrev Coin.T : Coin := 1
 
-abbrev CoinConfig := Fin (n + 1) → Coin
+abbrev CoinConfig (n : ℕ) := Fin n → Coin
 
-variable {n}
+def CoinConfig.countH {n : ℕ} (c : CoinConfig n) : ℕ := (List.ofFn c).count .H
 
-def CoinConfig.countH (c : CoinConfig n) : ℕ := (List.ofFn c).count .H
+def CoinConfig.flip {n : ℕ} (c : CoinConfig n) (k : ℕ) : CoinConfig n :=
+  fun i => if i.val + 1 = k then
+    match c i with
+    | .H => .T
+    | .T => .H
+  else c i
 
-def CoinConfig.flip (c : CoinConfig n) (k : Fin (n + 1)) : CoinConfig n :=
-  Function.update c k (c k).rev
-
-def CoinConfig.update (c : CoinConfig n) : Option (CoinConfig n) :=
+def CoinConfig.update {n : ℕ} (c : CoinConfig n) : Option (CoinConfig n) :=
   if c.countH = 0 then none else
-  .some <| c.flip ⟨c.countH.pred, by
-    have := (List.ofFn c).count_le_length .H
-    rw [Nat.lt_succ, Nat.pred_le_iff]
-    simpa only [List.length_ofFn] using (List.ofFn c).count_le_length .H⟩
+  .some <| c.flip c.countH
 
-def CoinConfig.updateMultipleTimes (c : CoinConfig n) : ℕ → Option (CoinConfig n)
+def CoinConfig.updateMultipleTimes {n : ℕ} (c : CoinConfig n) : ℕ → Option (CoinConfig n)
   | 0 =>
     if c.countH = 0 then none else .some c
   | k+1 => c.updateMultipleTimes k >>= update
 
-
-def CoinConfig.L (c : CoinConfig n) : ℕ := sorry
-
-def CoinConfig.L_spec (c : CoinConfig n) : c.updateMultipleTimes c.L = .none := sorry
-
-def CoinConfig.L_min (c : CoinConfig n) : ∀ (n : ℕ), n < c.L → c.updateMultipleTimes n ≠ .none := sorry
+def imo_2019_p5_b_solution : ℕ → ℚ := sorry
 
 /--
 The Bank of Bath issues coins with an $H$ on one side and a $T$ on the other. Harry has $n$ of these coins arranged in a line from left to right. He repeatedly performs the following operation: If there are exactly $k > 0$ coins showing $H$, then he turns over the $k^{th}$ coin from the left; otherwise, all coins show $T$ and he stops. For example, if $n = 3$ the process starting with the configuration $THT$ would be $THT \rightarrow HHT \rightarrow HTT \rightarrow TTT$, which stops after three operations. (a) Show that, for each initial configuration, Harry stops after a finite number of operations. (b) For each initial configuration $C$, let $L(C)$ be the number of operations before Harry stops. For example, $L(THT) = 3$ and $L(TTT) = 0$. Determine the average value of $L(C)$ over all $2^n$ possible initial configurations $C$.
 -/
-theorem imo_2019_p5_a : ∀ (c : CoinConfig n), ∃ N : ℕ, c.updateMultipleTimes N = .none := sorry
+theorem imo_2019_p5_a {n : ℕ} (hn : n > 0) : ∀ (c : CoinConfig n), ∃ N : ℕ, c.updateMultipleTimes N = .none := by sorry
 
-def imo_2019_p5_b_sol : ℕ → ℚ := sorry
-
-theorem imo_2019_p5_b : (imo_2019_p5_b_sol n = ∑ c : CoinConfig n, (c.L : ℚ) / (Fintype.card (CoinConfig n))) := sorry
+theorem imo_2019_p5_b {n : ℕ} (hn : n > 0) : (imo_2019_p5_b_solution n = ∑ c : CoinConfig n, (Nat.find (imo_2019_p5_a hn c) : ℚ) / (Fintype.card (CoinConfig n))) := by sorry
