42d7415054fe35b44ead1751ed8e61acf8609070
ahhwuhu <liujunqi@amss.ac.cn>
2025-03-19 22:43:38 +0800

    fix

--- CombiBench/imo_2019_p5.lean
+++ CombiBench/imo_2019_p5.lean
@@ -1,7 +1,5 @@
 import Mathlib
 
-variable (n : ℕ)
-
 abbrev Coin := Fin 2
 
 abbrev Coin.H : Coin := 0
@@ -20,15 +18,13 @@ def CoinConfig.flip {n : ℕ} (c : CoinConfig n) (k : ℕ) : CoinConfig n :=
   else c i
 
 def CoinConfig.update {n : ℕ} (c : CoinConfig n) : Option (CoinConfig n) :=
-  if c.countH = 0 then none else
-  .some <| c.flip c.countH
+  if c.countH = 0 then none else .some <| c.flip c.countH
 
 def CoinConfig.updateMultipleTimes {n : ℕ} (c : CoinConfig n) : ℕ → Option (CoinConfig n)
-  | 0 =>
-    if c.countH = 0 then none else .some c
+  | 0 => if c.countH = 0 then none else .some c
   | k+1 => c.updateMultipleTimes k >>= update
 
-def imo_2019_p5_b_solution : ℕ → ℚ := sorry
+abbrev imo_2019_p5_b_solution : ℕ → ℚ := sorry
 
 /--
 The Bank of Bath issues coins with an $H$ on one side and a $T$ on the other. Harry has $n$ of these coins arranged in a line from left to right. He repeatedly performs the following operation: If there are exactly $k > 0$ coins showing $H$, then he turns over the $k^{th}$ coin from the left; otherwise, all coins show $T$ and he stops. For example, if $n = 3$ the process starting with the configuration $THT$ would be $THT \rightarrow HHT \rightarrow HTT \rightarrow TTT$, which stops after three operations. (a) Show that, for each initial configuration, Harry stops after a finite number of operations. (b) For each initial configuration $C$, let $L(C)$ be the number of operations before Harry stops. For example, $L(THT) = 3$ and $L(TTT) = 0$. Determine the average value of $L(C)$ over all $2^n$ possible initial configurations $C$.
