8b2bac3c9be06bae2a772cf64ec9741f809f2152
ahhwuhu <liujunqi@amss.ac.cn>
2025-03-20 15:30:50 +0800

    draft

--- CombiBench/imo_2014_p6.lean
+++ CombiBench/imo_2014_p6.lean
@@ -1,6 +1,21 @@
 import Mathlib
 
+def General_position (L : Finset (ℝ × ℝ × ℝ)) : Prop :=
+  (∀ l ∈ L, l.1 ≠ 0 ∨ l.2.1 ≠ 0) ∧
+  (∀ l1 ∈ L, ∀ l2 ∈ L, l1 ≠ l2 → l1.1 * l2.2.1 ≠ l1.2.1 * l2.1) ∧
+  (∀ l1 ∈ L, ∀ l2 ∈ L, ∀ l3 ∈ L, l1 ≠ l2 → l1 ≠ l3 → l2 ≠ l3 →
+    (¬ ∃ (p : ℝ × ℝ), l1.1 * p.1 + l1.2.1 * p.2 + l1.2.2 = 0 ∧
+      l2.1 * p.1 + l2.2.1 * p.2 + l2.2.2 = 0 ∧ l3.1 * p.1 + l3.2.1 * p.2 + l3.2.2 = 0))
+
+def finite_regions (S : Set (ℝ × ℝ)) (L : Finset (ℝ × ℝ × ℝ)) : Prop :=
+  S ≠ ∅ ∧ (∃ a b : ℝ, ∀ p ∈ S, |p.1| ≤ a ∧ |p.2| ≤ b) ∧ (∀ p ∈ S, ∀ q ∈ S, p ≠ q → ∀ l ∈ L,
+    Real.sign (l.1 * p.1 + l.2.1 * p.2 + l.2.2) = Real.sign (l.1 * q.1 + l.2.1 * q.2 + l.2.2))
+
 /--
 A set of lines in the plane is in $\textit{general position}$ if no two are parallel and no three pass through the same point. A set of lines in general position cuts the plane into regions, some of which have finite area; we call these its $\textit{finite regions}$. Prove that for all sufficiently large $n$, in any set of $n$ lines in general position it is possible to colour at least $\sqrt{n}$ of the lines blue in such a way that none of its finite regions has a completely blue boundary.
 -/
-theorem imo_2014_p6 : 1 = 1 := sorry
+theorem imo_2014_p6 : ∀ᶠ n in Filter.atTop, ∀ L : Finset (ℝ × ℝ × ℝ), General_position L ∧ L.card = n →
+  ∃ B : Finset (ℝ × ℝ × ℝ), B ≤ L ∧ B.card ≥ Nat.sqrt n ∧
+  ∀ S, finite_regions S L := by
+
+  sorry
